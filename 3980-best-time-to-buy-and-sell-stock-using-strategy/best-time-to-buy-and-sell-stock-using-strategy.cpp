7a38a427ed79c8fb6a3ddff7b4a9d66fb059cd35
SouravAgarwal69 <souravagarwal22072003@gmail.com>
2026-02-01 16:43:20 +0530

    Time: 12 ms (58.51%) | Memory: 302.5 MB (99.96%) - LeetSync

--- 3980-best-time-to-buy-and-sell-stock-using-strategy/best-time-to-buy-and-sell-stock-using-strategy.cpp
+++ 3980-best-time-to-buy-and-sell-stock-using-strategy/best-time-to-buy-and-sell-stock-using-strategy.cpp
@@ -1,28 +1,28 @@
 class Solution {
 public:
     long long maxProfit(vector<int>& prices, vector<int>& strategy, int k) {
-        long long initialProfit=0,windowOriginal=0,windowModified=0,maxProfit=0;
+        long long initialCost=0,originalWindow=0,modifiedWindow=0,maxProfit=0;
         for(int i=0;i<prices.size();i++)
         {
-            initialProfit+=prices[i]*strategy[i];
+            initialCost+=prices[i]*strategy[i];
         }
         int i=0,j=0;
         while(j<prices.size())
         {
-            windowOriginal+=prices[j]*strategy[j];
-            if(j-i+1>k/2)
+            originalWindow+=prices[j]*strategy[j];
+            if(j-i>=k/2)
             {
-                windowModified+=prices[j];
+                modifiedWindow+=prices[j];
             }
             if(j-i+1==k)
             {
-                maxProfit=max(maxProfit,windowModified-windowOriginal);
-                windowOriginal-=prices[i]*strategy[i];
-                windowModified-=prices[i+k/2];
+                maxProfit=max(maxProfit,modifiedWindow-originalWindow);
+                originalWindow-=prices[i]*strategy[i];
+                modifiedWindow-=prices[i+(k/2)];
                 i++;
             }
             j++;
         }
-       return initialProfit+maxProfit;
+        return initialCost+maxProfit;
     }
 };
