cfc53f3047d9b5c5fbbe022be530c340f5ea898e
SouravAgarwal69 <souravagarwal22072003@gmail.com>
2026-02-03 06:00:35 +0530

    Time: 0 ms (100.00%) | Memory: 18.5 MB (49.86%) - LeetSync

---
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
@@ -0,0 +1,52 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* merge(ListNode*l1,ListNode*l2)
+    {
+        if(!l1)
+        {
+            return l2;
+        }
+        if(!l2)
+        {
+            return l1;
+        }
+        if(l1->val<=l2->val)
+        {
+            l1->next=merge(l1->next,l2);
+            return l1;
+        }
+        else
+        {
+            l2->next=merge(l1,l2->next);
+            return l2;
+        }
+    }
+    ListNode* Divide(vector<ListNode*>&lists,int s,int e)
+    {
+        if(lists.size()==0)
+        {
+            return NULL;
+        }
+        if(s==e)
+        {
+            return lists[s];
+        }
+        int mid=s+(e-s)/2;
+        ListNode*l1=Divide(lists,s,mid);
+        ListNode*l2=Divide(lists,mid+1,e);
+        return merge(l1,l2);
+    }
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        return Divide(lists,0,lists.size()-1);
+    }
+};
