55412127440358d0bfd80ba75539180fc09f45e0
SouravAgarwal69 <souravagarwal22072003@gmail.com>
2026-02-04 16:19:04 +0530

    Time: 0 ms (100.00%) | Memory: 76.9 MB (82.04%) - LeetSync

--- 445-add-two-numbers-ii/add-two-numbers-ii.cpp
+++ 445-add-two-numbers-ii/add-two-numbers-ii.cpp
@@ -10,42 +10,41 @@
  */
 class Solution {
 public:
-   ListNode* reverseLL(ListNode*head)
-   {
-      ListNode*prev=NULL,*curr=head;
-      while(curr)
-      {
-        ListNode*forward=curr->next;
-        curr->next=prev;
-        prev=curr;
-        curr=forward;
-      }
-      return prev;
-   }
-    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        l1=reverseLL(l1);
-        l2=reverseLL(l2);
-        ListNode*ans=new ListNode();
-        int sum=0,carry=0;
-        while(l1 || l2)
+    ListNode*reverseLL(ListNode*head)
+    {
+        if(!head || !head->next)
         {
-            if(l1)
-            {
-                sum+=l1->val;
-                l1=l1->next;
-            }
-            if(l2)
-            {
-                sum+=l2->val;
-                l2=l2->next;
-            }
-            ans->val=sum%10;
-            carry=sum/10;
-            sum=carry;
-            ListNode* NewNode=new ListNode(carry);
-            NewNode->next=ans;
-            ans=NewNode;
+            return head;
         }
-        return ans->val==0?ans->next:ans;
+      ListNode*revHead=reverseLL(head->next);
+      head->next->next=head;
+      head->next=NULL;
+      return revHead;
+    }
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+      l1=reverseLL(l1);
+      l2=reverseLL(l2);
+      ListNode*dummy=new ListNode();
+      ListNode*curr=dummy;
+      int sum=0,carry=0;
+      while(l1 || l2 || carry!=0)
+      {
+         if(l1)
+         {
+           sum+=l1->val;
+           l1=l1->next;
+         }
+         if(l2)
+         {
+            sum+=l2->val;
+            l2=l2->next;
+         }
+         ListNode* temp=new ListNode(sum%10);
+         curr->next=temp;
+         curr=temp;
+         carry=sum/10;
+         sum=carry;
+      }
+      return reverseLL(dummy->next);
     }
 };
