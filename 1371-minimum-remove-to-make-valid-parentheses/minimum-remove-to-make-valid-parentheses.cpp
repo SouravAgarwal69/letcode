eebe01997e8210d4698b6196714b9869b4bf50dd
SouravAgarwal69 <souravagarwal22072003@gmail.com>
2026-02-11 07:44:16 +0530

    Time: 15 ms (20.49%) | Memory: 14.6 MB (38.64%) - LeetSync

--- 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.cpp
+++ 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.cpp
@@ -1,30 +1,43 @@
 class Solution {
 public:
     string minRemoveToMakeValid(string s) {
-       string ans;
-       vector<int>index;
-       for(int i=0;i<s.size();i++)
-       {
-          if( !index.empty() && s[i]==')' && s[index.back()]=='(')
-          {
-              index.pop_back();
-          }
-          else if(s[i]=='(' || s[i]==')')
-          {
-            index.push_back(i);
-          }
-       } 
-       int k=0;
-      for(int i=0;i<s.size();i++)
-      { 
-         if( k<index.size() && i==index[k])
-         {
-            k++;
-            continue;
-         }
-         ans.push_back(s[i]);
-         
-      }
-       return ans;
+        string ans;
+        int open=0;
+        for(int i=0;i<s.size();i++)
+        {
+            if(s[i]=='(')
+            {
+                open++;
+            }
+            else if(s[i]==')' && open==0)
+            {
+                 continue;
+            }
+            else if(s[i]==')' && open>0)
+            {
+                open--;
+            }
+           ans.push_back(s[i]);
+        }
+        string result;
+        int close=0;
+        for(int i=ans.size()-1;i>=0;i--)
+        {
+            if(ans[i]==')')
+            {
+                close++;
+            }
+            else if(ans[i]=='(' &&  close>0 )
+            {
+                close--;
+            }
+            else if(ans[i]=='(' && close==0)
+            {
+                continue;
+            }
+            result.push_back(ans[i]);
+        }
+        reverse(result.begin(),result.end());
+        return result;
     }
 };
