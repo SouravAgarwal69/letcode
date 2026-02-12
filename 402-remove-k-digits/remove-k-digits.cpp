bb5e22c8123ebc14e8c0b42ada522bdb623b08ae
SouravAgarwal69 <souravagarwal22072003@gmail.com>
2026-02-12 16:17:01 +0530

    Time: 5 ms (35.70%) | Memory: 11 MB (94.89%) - LeetSync

--- 402-remove-k-digits/remove-k-digits.cpp
+++ 402-remove-k-digits/remove-k-digits.cpp
@@ -4,18 +4,18 @@ public:
         string ans;
         for(int i=0;i<num.size();i++)
         {
-            while(!ans.empty() && ans.back()>num[i] && k>0)
+            while(!ans.empty() && ans.back()-'0'>num[i]-'0' && k>0)
             {
-                ans.pop_back();
-                k--;
+                 ans.pop_back();
+                 k--;
             }
-           if(ans.empty() && num[i]=='0')
-           {
-             continue;
-           }
+            if(ans.empty() && num[i]=='0')
+            {
+                continue;
+            }
            ans.push_back(num[i]);
         }
-        while(k>0  && !ans.empty())
+        while(k>0 && !ans.empty())
         {
             ans.pop_back();
             k--;
