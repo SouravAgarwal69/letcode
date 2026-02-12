9288bad44a4253f7de5978dea5340cacbbd90199
SouravAgarwal69 <souravagarwal22072003@gmail.com>
2026-02-12 21:50:31 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (73.81%) - LeetSync

---
+++ 1298-reverse-substrings-between-each-pair-of-parentheses/reverse-substrings-between-each-pair-of-parentheses.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    string reverseParentheses(string s) {
+        string result;
+        stack<int>st;
+        for(int i=0;i<s.size();i++)
+        {
+            if(s[i]=='(')
+            {
+                st.push(result.size());
+            }
+            else if(s[i]==')')
+            {
+                reverse(result.begin()+st.top(),result.end());
+                st.pop();
+            }
+           else
+           {
+              result.push_back(s[i]);
+           }
+        }
+        return result;
+    }
+};
