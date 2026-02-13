f62748ce1763074f1479b73803fcdc7fc653dced
SouravAgarwal69 <souravagarwal22072003@gmail.com>
2026-02-13 13:51:37 +0530

    Time: 46 ms (52.17%) | Memory: 27.2 MB (61.30%) - LeetSync

--- 1818-maximum-score-from-removing-substrings/maximum-score-from-removing-substrings.cpp
+++ 1818-maximum-score-from-removing-substrings/maximum-score-from-removing-substrings.cpp
@@ -2,37 +2,36 @@ class Solution {
 public:
     int maximumGain(string s, int x, int y) {
         int totalGain=0;
-        int maxScore=max(x,y);
-        int minScore=min(x,y);
         string maxString=x>y?"ab":"ba";
         string minString=x>y?"ba":"ab";
+        int maxScore=max(x,y);
+        int minScore=min(x,y);
         string result;
         for(int i=0;i<s.size();i++)
         {
             if(!result.empty() && result.back()==maxString[0] && s[i]==maxString[1])
             {
-                totalGain+=maxScore;
                 result.pop_back();
+                totalGain+=maxScore;
             }
             else
             {
-                result.push_back(s[i]);
+               result.push_back(s[i]);   
             }
         }
-        int size=result.size();
         string temp;
-        for(int i=0;i<size;i++)
+        for(int i=0;i<result.size();i++)
         {
             if(!temp.empty() && temp.back()==minString[0] && result[i]==minString[1])
             {
-                totalGain+=minScore;
                 temp.pop_back();
+                totalGain+=minScore;
             }
-            else{
+            else
+            {
                 temp.push_back(result[i]);
             }
         }
         return totalGain;
-
     }
 };
