ab1baeee424ecc96d502c59172c9ad2e908ef381
SouravAgarwal69 <souravagarwal22072003@gmail.com>
2026-02-14 15:47:08 +0530

    Time: 0 ms (100.00%) | Memory: 10.2 MB (51.21%) - LeetSync

--- 726-number-of-atoms/number-of-atoms.cpp
+++ 726-number-of-atoms/number-of-atoms.cpp
@@ -14,10 +14,10 @@ public:
             }
             else if(formula[i]==')')
             {
-                unordered_map<string,int>mp=st.top();
+                unordered_map<string,int>curr=st.top();
                 st.pop();
-                string digit;
                 i++;
+                string digit;
                 while(i<formula.size() && isdigit(formula[i]))
                 {
                     digit+=formula[i];
@@ -26,38 +26,37 @@ public:
                 if(!digit.empty())
                 {
                     int multiplier=stoi(digit);
-                    for(auto it=mp.begin();it!=mp.end();it++)
+                    for(auto it=curr.begin();it!=curr.end();it++)
                     {
                         it->second=(it->second)*multiplier;
                     }
                 }
-                for(auto it=mp.begin();it!=mp.end();it++)
+                for(auto it=curr.begin();it!=curr.end();it++)
                 {
                     st.top()[it->first]+=it->second;
                 }
             }
             else
             {
-                string temp;
-                temp+=formula[i];
-                i++;
-                while(i<formula.size() && islower(formula[i]))
-                {
+                 string temp;
+                 temp+=formula[i];
+                 i++;
+                 while(i<formula.size() && islower(formula[i]))
+                 {
                     temp+=formula[i];
                     i++;
-                }
-                string digit;
-                while(i<formula.size() && isdigit(formula[i]))
-                {
+                 }
+                 string digit;
+                 while(i<formula.size() && isdigit(formula[i]))
+                 {
                     digit+=formula[i];
                     i++;
-                }
-                int num=digit.empty()?1:stoi(digit);
-                st.top()[temp]+=num;
+                 }
+                 st.top()[temp]+=digit.empty()?1:stoi(digit);
             }
         }
         map<string,int>mp(st.top().begin(),st.top().end());
-        for(auto it=mp.begin();it!=mp.end();it++)
+        for(auto it =mp.begin();it!=mp.end();it++)
         {
             result+=it->first;
             if(it->second>1)
