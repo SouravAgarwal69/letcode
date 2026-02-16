ef6410fbea7d09051604eee2d974e3071cd92052
SouravAgarwal69 <souravagarwal22072003@gmail.com>
2026-02-16 09:43:36 +0530

    Time: 17 ms (27.62%) | Memory: 30 MB (79.29%) - LeetSync

--- 2221-check-if-a-parentheses-string-can-be-valid/check-if-a-parentheses-string-can-be-valid.cpp
+++ 2221-check-if-a-parentheses-string-can-be-valid/check-if-a-parentheses-string-can-be-valid.cpp
@@ -1,44 +1,49 @@
 class Solution {
 public:
     bool canBeValid(string s, string locked) {
+        int open=0,close=0;
         if(s.size()%2!=0)
         {
             return false;
         }
-        stack<int>flexible;
-        stack<int>open;
         for(int i=0;i<s.size();i++)
         {
             if(locked[i]=='0')
             {
-                flexible.push(i);
+                open++;
             }
             else if(s[i]=='(')
             {
-                open.push(i);
+                open++;
             }
-            else if(s[i]==')' && !open.empty())
+            else if(s[i]==')')
             {
-                open.pop();
-            }
-            else if(s[i]==')' && !flexible.empty())
-            {
-                flexible.pop();
-            }
-            else
-            {
-                return false;
+                open--;
+                if(open<0)
+                {
+                    return false;
+                }
             }
         }
-        while(!open.empty() && !flexible.empty())
+        for(int i=s.size()-1;i>=0;i--)
         {
-            if(open.top()>flexible.top())
+            if(locked[i]=='0')
             {
-                 return false;
+                close++;
             }
-            open.pop();
-            flexible.pop();
+            else if(s[i]==')')
+            {
+                close++;
+            }
+            else if(s[i]=='(')
+            {
+                close--;
+                if(close<0)
+                {
+                    return false;
+                }
+            }
         }
-        return open.empty();
+        return true;
     }
 };
