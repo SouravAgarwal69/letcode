319d8055b3f869d32de5f475d2fe6a8fe926d3e5
SouravAgarwal69 <souravagarwal22072003@gmail.com>
2026-02-21 18:50:43 +0530

    Time: 1 ms (39.00%) | Memory: 21.7 MB (70.57%) - LeetSync

--- 735-asteroid-collision/asteroid-collision.cpp
+++ 735-asteroid-collision/asteroid-collision.cpp
@@ -5,27 +5,27 @@ public:
         for(int i=0;i<asteroids.size();i++)
         {
             bool collision=false;
-            while(!result.empty() && asteroids[i]<0 && result.back()>0)
+            while(!result.empty() && result.back()>0 && asteroids[i]<0)
             {
-               if(abs(asteroids[i])==result.back())
-               {
-                  result.pop_back();
-                  collision=true;
-                  break;
-               }
-               else if(abs(asteroids[i])>result.back())
-               {
-                  result.pop_back();
-               }
-               else
-               {
-                  collision=true;
-                  break;
-               }
+                if(abs(asteroids[i])==result.back())
+                {
+                    collision=true;
+                    result.pop_back();
+                    break;
+                }
+                else if(abs(asteroids[i])>result.back())
+                {
+                    result.pop_back();
+                }
+                else
+                {
+                    collision=true;
+                    break;
+                }
             }
             if(!collision)
             {
-                result.push_back(asteroids[i]);
+               result.push_back(asteroids[i]);
             }
         }
         return result;
