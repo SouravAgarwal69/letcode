805a7a5269d4ab63d190eec76fe887d10d93e1a9
SouravAgarwal69 <souravagarwal22072003@gmail.com>
2026-02-21 12:48:55 +0530

    Time: 0 ms (100.00%) | Memory: 13.5 MB (58.07%) - LeetSync

--- 224-basic-calculator/basic-calculator.cpp
+++ 224-basic-calculator/basic-calculator.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     int calculate(string s) {
-        int result=0,sign=1;
-        long long num=0;
+        long long result=0,sign=1;
         stack<int>st;
+        long long num=0;
         for(int i=0;i<s.size();i++)
         {
             if(s[i]=='(')
@@ -13,16 +13,6 @@ public:
                 result=0;
                 sign=1;
             }
-            else if(isdigit(s[i]))
-            {
-                num=num*10+s[i]-'0';
-            }
-            else if(s[i]=='+' || s[i]=='-')
-            {
-                result+=num*sign;
-                num=0;
-                sign=s[i]=='+'?1:-1;
-            }
             else if(s[i]==')')
             {
                 result+=num*sign;
@@ -30,7 +20,17 @@ public:
                 st.pop();
                 int prevVal=st.top();
                 st.pop();
-                result=prevVal+prevSign*result;
+                result=prevSign*result+prevVal;
+                num=0;
+            }
+            else if (isdigit(s[i]))
+            {
+                num=num*10+(s[i]-'0');
+            }
+            else if(s[i]=='+' || s[i]=='-')
+            {
+                result+=num*sign;
+                sign=s[i]=='+'?1:-1;
                 num=0;
             }
         }
