c2385305e3bd4a3c487221333ae721dc7cab2419
SouravAgarwal69 <souravagarwal22072003@gmail.com>
2026-02-21 16:19:32 +0530

    Time: 23 ms (34.01%) | Memory: 28.5 MB (61.90%) - LeetSync

--- 2470-removing-stars-from-a-string/removing-stars-from-a-string.cpp
+++ 2470-removing-stars-from-a-string/removing-stars-from-a-string.cpp
@@ -4,13 +4,13 @@ public:
         string result;
         for(int i=0;i<s.size();i++)
         {
-            if(s[i]=='*' && !result.empty())
+            if(!result.empty() && s[i]=='*')
             {
                 result.pop_back();
             }
             else
             {
-                  result.push_back(s[i]);
+                result.push_back(s[i]);
             }
         }
         return result;
