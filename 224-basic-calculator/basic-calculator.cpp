312e82668ca5854864250fab208bd32ae73bb51e
SouravAgarwal69 <souravagarwal22072003@gmail.com>
2026-02-21 12:32:53 +0530

    Time: 3 ms (58.80%) | Memory: 12.4 MB (96.88%) - LeetSync

--- 224-basic-calculator/basic-calculator.cpp
+++ 224-basic-calculator/basic-calculator.cpp
@@ -1,41 +1,40 @@
 class Solution {
 public:
     int calculate(string s) {
-        stack<int>st;
         int result=0,sign=1;
-         long long num=0;
+        long long num=0;
+        stack<int>st;
         for(int i=0;i<s.size();i++)
         {
-            if(isdigit(s[i]))
-            {
-                num=num*10+s[i]-'0';
-            }
-            if(s[i]=='+' || s[i]=='-')
-            {
-                result+=sign*num;
-                num=0;
-                sign=s[i]=='+'?1:-1;
-            }
             if(s[i]=='(')
             {
                 st.push(result);
                 st.push(sign);
                 result=0;
                 sign=1;
-                num=0;
             }
-            if(s[i]==')')
+            else if(isdigit(s[i]))
+            {
+                num=num*10+s[i]-'0';
+            }
+            else if(s[i]=='+' || s[i]=='-')
+            {
+                result+=num*sign;
+                num=0;
+                sign=s[i]=='+'?1:-1;
+            }
+            else if(s[i]==')')
             {
                 result+=num*sign;
                 int prevSign=st.top();
                 st.pop();
                 int prevVal=st.top();
                 st.pop();
-                result=prevVal+result*prevSign;
+                result=prevVal+prevSign*result;
                 num=0;
             }
         }
-         result+=sign*num;
-         return result;
+        result+=num*sign;
+        return result;
     }
 };
