29651a14fd6ac6dc4425aace2e9f9da1d8282fe0
SouravAgarwal69 <souravagarwal22072003@gmail.com>
2026-02-21 15:15:11 +0530

    Time: 15 ms (68.17%) | Memory: 47.4 MB (20.50%) - LeetSync

--- 943-sum-of-subarray-minimums/sum-of-subarray-minimums.cpp
+++ 943-sum-of-subarray-minimums/sum-of-subarray-minimums.cpp
@@ -1,48 +1,58 @@
 class Solution {
 public:
-    int mod=1e9+7;
+   int mod=1e9+7;
+    void findNSL(vector<int>&NSL,vector<int>&arr)
+    {
+        stack<int>st;
+        for(int i=0;i<arr.size();i++)
+        {
+            while(!st.empty() && arr[st.top()]>=arr[i])
+            {
+               st.pop();
+            }
+            if(st.empty())
+            {
+                NSL[i]=-1;
+            }
+            else
+            {
+                NSL[i]=st.top();
+            }
+            st.push(i);
+        }
+    }
+    void findNSR(vector<int>&NSR,vector<int>&arr)
+    {
+        stack<int>st;
+        for(int i=arr.size()-1;i>=0;i--)
+        {
+            while(!st.empty() && arr[st.top()]>arr[i])
+            {
+                st.pop();
+            }
+            if(st.empty())
+            {
+                NSR[i]=arr.size();
+            }
+            else
+            {
+                NSR[i]=st.top();
+            }
+            st.push(i);
+        }
+    }
     int sumSubarrayMins(vector<int>& arr) {
-       long long totalSum=0;
-       vector<int>leftIndex(arr.size());
-       vector<int>rightIndex(arr.size());
-       stack<int>leftSt;
-       stack<int>rightSt;
-       for(int i=0;i<arr.size();i++)
-       {
-          while(!leftSt.empty() && arr[leftSt.top()]>=arr[i])
-          {
-               leftSt.pop();          
-          }
-          if(leftSt.empty())
-          {
-            leftIndex[i]=-1;
-          }
-          else
-          {
-            leftIndex[i]=leftSt.top();
-          }
-          leftSt.push(i);
-       }
-       for(int i=arr.size()-1;i>=0;i--)
-       {
-           while(!rightSt.empty() && arr[rightSt.top()]>arr[i])
-           {
-               rightSt.pop();
-           }
-           if(rightSt.empty())
-           {
-               rightIndex[i]=arr.size();
-           }
-           else
-           {
-                rightIndex[i]=rightSt.top();
-           }
-           rightSt.push(i);
-       }
-       for(int i=0;i<arr.size();i++)
-       {
-         totalSum+=1LL*arr[i]*((i-leftIndex[i])*(rightIndex[i]-i));
-       }
-       return totalSum %mod;
+        long long total=0;
+        vector<int>NSL(arr.size());
+        vector<int>NSR(arr.size());
+        findNSL(NSL,arr);
+        findNSR(NSR,arr);
+        for(int i=0;i<arr.size();i++)
+        {
+            int d1=i-NSL[i];
+            int d2=NSR[i]-i;
+            total=total+(1LL*d1*d2*arr[i])%mod;
+        }
+        return total%mod;
     }
 };
