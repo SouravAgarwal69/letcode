a7fb649bbce578bd931f6e3770790770737cf8f2
SouravAgarwal69 <souravagarwal22072003@gmail.com>
2026-02-21 18:25:33 +0530

    Time: 0 ms (100.00%) | Memory: 19.1 MB (12.27%) - LeetSync

--- 983-validate-stack-sequences/validate-stack-sequences.cpp
+++ 983-validate-stack-sequences/validate-stack-sequences.cpp
@@ -2,15 +2,16 @@ class Solution {
 public:
     bool validateStackSequences(vector<int>& pushed, vector<int>& popped) {
         stack<int>st;
-        int j=0;
-        for(int i=0;i<pushed.size();i++)
+        int i=0,j=0;
+        while(i<pushed.size() && j<popped.size())
         {
             st.push(pushed[i]);
-            while(!st.empty() && popped[j]==st.top())
+            while(!st.empty() && j<popped.size() && st.top()==popped[j])
             {
                 st.pop();
                 j++;
             }
+            i++;
         }
         return st.empty();
     }
