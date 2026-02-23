043e0a6e25ce3797dfd65377dc2fcf99b7b52283
SouravAgarwal69 <souravagarwal22072003@gmail.com>
2026-02-23 06:50:34 +0530

    Time: 0 ms (100.00%) | Memory: 20.5 MB (62.24%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.cpp
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     int jump(vector<int>& nums) {
-        int jump=0,currEnd=0,maxEnd=0;
+        int jumps=0,currEnd=0,maxEnd=0;
         for(int i=0;i<nums.size();i++)
         {
             if(i>currEnd)
             {
-                jump++;
+                jumps++;
                 currEnd=maxEnd;
             }
             maxEnd=max(maxEnd,nums[i]+i);
         }
-        return jump;
+        return jumps;
     }
 };
