8b29512cadefe033ad8f0567c9e0160be3fdbc75
SouravAgarwal69 <souravagarwal22072003@gmail.com>
2026-02-23 08:03:03 +0530

    Time: 0 ms (100.00%) | Memory: 23 MB (31.42%) - LeetSync

--- 135-candy/candy.cpp
+++ 135-candy/candy.cpp
@@ -1,26 +1,26 @@
 class Solution {
 public:
     int candy(vector<int>& ratings) {
-        int total=0;
         vector<int>temp(ratings.size(),1);
-        for(int i=1;i<ratings.size();i++)
+        for(int i=0;i<ratings.size()-1;i++)
         {
-            if(ratings[i]>ratings[i-1])
-            {
-                temp[i]=temp[i-1]+1;
-            }
+             if(ratings[i]<ratings[i+1])
+             {
+                temp[i+1]=max(temp[i]+1,temp[i+1]);
+             }
         }
         for(int i=ratings.size()-2;i>=0;i--)
         {
-            if(ratings[i]>ratings[i+1])
+            if(ratings[i+1]<ratings[i])
             {
                 temp[i]=max(temp[i+1]+1,temp[i]);
             }
         }
-        for(int i=0;i<ratings.size();i++)
+        int totalCandy=0;
+        for(int i=0;i<temp.size();i++)
         {
-            total+=temp[i];
+            totalCandy+=temp[i];
         }
-        return total;
+        return totalCandy;
     }
 };
