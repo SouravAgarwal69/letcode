5444e65cafdc1951c1f5801913da62b330c1220f
SouravAgarwal69 <souravagarwal22072003@gmail.com>
2026-03-02 06:07:33 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (17.78%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
-    int fun(int s,int target,vector<int>&dp)
+    int fun(int n,vector<int>&dp)
     {
-        if(s==target)
-       {
-           return 1;
-       }
-       if(s>target)
-       {
-          return 0;
-       }
-       if(dp[s]!=-1)
-       {
-          return dp[s];
-       }
-       return dp[s]=fun(s+1,target,dp)+fun(s+2,target,dp);
+        if(n<0)
+        {
+            return 0;
+        }
+        if(n==0)
+        {
+            return 1;
+        }
+        if(dp[n]!=-1)
+        {
+            return dp[n];
+        }
+        return dp[n]=fun(n-1,dp)+fun(n-2,dp);
     }
     int climbStairs(int n) {
         vector<int>dp(n+1,-1);
-        return fun(0,n,dp);
+        return fun(n,dp);
     }
 };
