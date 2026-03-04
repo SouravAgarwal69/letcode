9b22f028dbb24e02f8f16ecac5f0fa35cbbc5d3a
SouravAgarwal69 <souravagarwal22072003@gmail.com>
2026-03-04 13:17:57 +0530

    Time: 0 ms (100.00%) | Memory: 10.7 MB (53.11%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.cpp
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -1,23 +1,26 @@
 class Solution {
 public:
-     int find(vector<int>&nums,int s,int e)
-     {
-          int prevPrev=0,prev=nums[s],ans=nums[s];
-          for(int i=s+1;i<=e;i++)
-          {
-              int skip=prev;
-              int stashed=nums[i]+prevPrev;
-              ans=max(skip,stashed);
-              prevPrev=prev;
-              prev=ans;
-          }
-          return ans;
-     }
+    int find(int s,int e,vector<int>&nums,vector<int>&dp)
+    {
+        if(s>e)
+        {
+            return 0;
+        }
+       if(dp[s]!=-1)
+       {
+          return dp[s];
+       }
+       int skip=find(s+1,e,nums,dp);
+       int stashed=nums[s]+find(s+2,e,nums,dp);
+       return dp[s]=max(skip,stashed);
+    }
     int rob(vector<int>& nums) {
         if(nums.size()==1)
         {
             return nums[0];
         }
-       return max(find(nums,0,nums.size()-2),find(nums,1,nums.size()-1));
+        vector<int>dp1(nums.size(),-1);
+        vector<int>dp2(nums.size(),-1);
+        return max(find(0,nums.size()-2,nums,dp1),find(1,nums.size()-1,nums,dp2));
     }
 };
