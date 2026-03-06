7506a074f6b08b34ffbe4d9f82708d38acd33da7
SouravAgarwal69 <souravagarwal22072003@gmail.com>
2026-03-06 09:06:47 +0530

    Time: 270 ms (32.79%) | Memory: 157.1 MB (45.30%) - LeetSync

--- 2022-maximum-alternating-subsequence-sum/maximum-alternating-subsequence-sum.cpp
+++ 2022-maximum-alternating-subsequence-sum/maximum-alternating-subsequence-sum.cpp
@@ -1,22 +1,12 @@
 class Solution {
 public:
-    long long findMax(vector<int>&nums,int flag,int index,vector<vector<long long>>&dp)
-    {
-        if(index==nums.size())
-        {
-            return 0;
-        }
-       if(dp[index][flag]!=-1)
-       {
-          return dp[index][flag];
-       }
-       long long value=flag==1?(-1)*nums[index]:nums[index];
-        long long take=value+findMax(nums,1-flag,index+1,dp);
-        long long skip=findMax(nums,flag,index+1,dp);
-        return dp[index][flag]=max(take,skip);
-    }
     long long maxAlternatingSum(vector<int>& nums) {
-        vector<vector<long long>>dp(nums.size(),vector<long long>(2,-1));
-        return findMax(nums,0,0,dp);
+        vector<vector<long long>>dp(nums.size()+1,vector<long long>(2));
+        for(int i=1;i<=nums.size();i++)
+        {
+            dp[i][0]=max(dp[i-1][0],nums[i-1]+dp[i-1][1]);
+            dp[i][1]=max(dp[i-1][1],dp[i-1][0]-nums[i-1]);
+        }
+        return max(dp[nums.size()][0],dp[nums.size()][1]);
     }
 };
