2de773188cd799ed2f9e3c0de9f1af53875d851b
SouravAgarwal69 <souravagarwal22072003@gmail.com>
2026-03-09 22:21:00 +0530

    Time: 0 ms (100.00%) | Memory: 10.2 MB (88.20%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.cpp
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -1,29 +1,23 @@
 class Solution {
 public:
-     int dp[101];
-    int find(int index,int n,vector<int>&nums)
+    int find(vector<int>&nums,int s,int e)
     {
-        if(index>n)
+        int prevPrev=0,prev=nums[s],ans=nums[s];
+        for(int i=s+1;i<=e;i++)
         {
-            return 0;
+           int take=nums[i]+prevPrev;
+           int skip=prev;
+           ans=max(skip,take);
+           prevPrev=prev;
+           prev=ans;
         }
-        if(dp[index]!=-1)
-        {
-            return dp[index];
-        }
-        int take= nums[index]+find(index+2,n,nums);
-        int skip= find(index+1,n,nums);
-        return dp[index]=max(take,skip);
+        return ans;
     }
     int rob(vector<int>& nums) {
         if(nums.size()==1)
         {
             return nums[0];
         }
-        memset(dp,-1,sizeof(dp));
-        int case1=find(0,nums.size()-2,nums);
-        memset(dp,-1,sizeof(dp));
-        int case2=find(1,nums.size()-1,nums);
-        return max(case1,case2);
+        return max(find(nums,0,nums.size()-2),find(nums,1,nums.size()-1));
     }
 };
