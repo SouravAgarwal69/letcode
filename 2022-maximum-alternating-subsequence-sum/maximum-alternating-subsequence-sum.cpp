88b9259f427bfcab5e1ae56b17a70536ca2c8fde
SouravAgarwal69 <souravagarwal22072003@gmail.com>
2026-03-10 19:47:48 +0530

    Time: 115 ms (92.17%) | Memory: 96.6 MB (85.29%) - LeetSync

--- 2022-maximum-alternating-subsequence-sum/maximum-alternating-subsequence-sum.cpp
+++ 2022-maximum-alternating-subsequence-sum/maximum-alternating-subsequence-sum.cpp
@@ -1,23 +1,16 @@
 class Solution {
 public:
-    long long dp[100000][2];
-    long long find(int index,vector<int>&nums,int flag)
-    {
-        if(index==nums.size())
-        {
-            return 0;
-        }
-       if(dp[index][flag]!=-1)
-        {
-            return dp[index][flag];
-        }
-        int value=flag==0?nums[index]:-nums[index];
-        long long take=value+find(index+1,nums,1-flag);
-        long long skip=find(index+1,nums,flag);
-        return dp[index][flag]=max(take,skip);
-    }
     long long maxAlternatingSum(vector<int>& nums) {
-        memset(dp,-1,sizeof(dp));
-        return find(0,nums,0);
+        long long dp[nums.size()+1][2];
+        dp[0][0]=0;
+        dp[0][1]=0;
+        long long ans=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            dp[i+1][0]=max(dp[i][0],dp[i][1]+nums[i]);
+            dp[i+1][1]=max(dp[i][1],dp[i][0]-nums[i]);
+            ans=max(dp[i+1][0],dp[i+1][1]);
+        }
+        return ans;
     }
 };
