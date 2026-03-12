a77ee2dcc113ff2adbbe2a325ef78a6a95b77b0f
SouravAgarwal69 <souravagarwal22072003@gmail.com>
2026-03-12 08:00:41 +0530

    Time: 0 ms (100.00%) | Memory: 95.1 MB (80.28%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
@@ -1,37 +1,71 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        vector<int>temp;
-        int i=0,j=0;
+        int element1=-1,element2=-1;
+        int size=nums1.size()+nums2.size();
+        int i=0,j=0,k=0,index1=(size-1)/2,index2=size/2;
         while(i<nums1.size() && j<nums2.size())
         {
-            if(nums1[i]<=nums2[j])
+            if(nums1[i]<nums2[j])
             {
-                temp.push_back(nums1[i]);
-                i++;
+               
+                 if(k==index1)
+            {
+                element1=nums1[i];
+            }
+            if(k==index2)
+            {
+                element2=nums1[i];
+            }
+             i++;
+             k++;
             }
             else
             {
-                temp.push_back(nums2[j]);
-                j++;
+               
+                 if(k==index1)
+            {
+                element1=nums2[j];
+            }
+            if(k==index2)
+            {
+                element2=nums2[j];
+            }
+             j++;
+                k++;
             }
         }
         while(i<nums1.size())
         {
-            temp.push_back(nums1[i]);
-            i++;
+           
+            if(k==index1)
+            {
+                element1=nums1[i];
+            }
+            if(k==index2)
+            {
+                element2=nums1[i];
+            }
+             i++;
+            k++;
         }
         while(j<nums2.size())
         {
-            temp.push_back(nums2[j]);
-            j++;
+             if(k==index1)
+            {
+                element1=nums2[j];
+            }
+            if(k==index2)
+            {
+                element2=nums2[j];
+            }
+             j++;
+            k++;
         }
-        int mid1=temp[(temp.size()-1)/2];
-        int mid2=temp[(temp.size())/2];
-        if(temp.size()%2==0)
+        if((nums1.size()+nums2.size())%2==0)
         {
-            return (mid1+mid2)/2.0;
+            return (element1+element2)/2.0;
         }
-        return mid1;
+        return element1;
     }
 };
