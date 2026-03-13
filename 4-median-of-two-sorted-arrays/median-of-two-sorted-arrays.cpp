48b766914b41a1fdc32cf786c262995e8ddde575
SouravAgarwal69 <souravagarwal22072003@gmail.com>
2026-03-14 03:58:15 +0530

    Time: 0 ms (100.00%) | Memory: 95 MB (80.29%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
@@ -5,30 +5,32 @@ public:
         {
             swap(nums1,nums2);
         }
-        int s=0,e=nums1.size(),size=nums1.size()+nums2.size();
+        int size=nums1.size()+nums2.size();
+        int s=0,e=nums1.size();
         while(s<=e)
         {
-            int p1=s+(e-s)/2;
-            int p2=((size+1)/2)-p1;
-            double x1=p1==0?INT_MIN:nums1[p1-1];
-            double x2=p1==nums1.size()?INT_MAX:nums1[p1];
-            double y1=p2==0?INT_MIN:nums2[p2-1];
-            double y2=p2==nums2.size()?INT_MAX:nums2[p2];
-            if(x1<=y2 && y1<=x2)
-            {
-                if(size%2==0)
-                {
-                    return (max(x1,y1)+min(x2,y2))/2.0;
-                }
-                return max(x1,y1);
-            }
-            else if(x1>y2)
-            {
-                e=p1-1;
-            }
-            else{
-                s=p1+1;
-            }
+           int p1=s+(e-s)/2;
+           int p2=((size+1)/2)-p1;
+           int x1=p1==0?INT_MIN:nums1[p1-1];
+           int x2=p1==nums1.size()?INT_MAX:nums1[p1];
+           int y1=p2==0?INT_MIN:nums2[p2-1];
+           int y2=p2==nums2.size()?INT_MAX:nums2[p2];
+           if(x1<=y2 && y1<=x2)
+           {
+              if(size%2==0)
+              {
+                return (max(x1,y1)+min(x2,y2))/2.0;
+              }
+              return max(x1,y1);
+           }
+           else if(x1>y2)
+           {
+              e=p1-1;
+           }
+          else{
+            s=p1+1;
+          }
+
         }
         return -1;
     }
