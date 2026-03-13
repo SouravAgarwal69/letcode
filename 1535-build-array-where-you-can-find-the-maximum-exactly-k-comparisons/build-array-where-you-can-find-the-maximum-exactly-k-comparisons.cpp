624f3c906a43b6c7af636c2c6d1292c0a9e1f8cc
SouravAgarwal69 <souravagarwal22072003@gmail.com>
2026-03-13 08:12:02 +0530

    Time: 147 ms (27.21%) | Memory: 8.8 MB (99.32%) - LeetSync

--- 1535-build-array-where-you-can-find-the-maximum-exactly-k-comparisons/build-array-where-you-can-find-the-maximum-exactly-k-comparisons.cpp
+++ 1535-build-array-where-you-can-find-the-maximum-exactly-k-comparisons/build-array-where-you-can-find-the-maximum-exactly-k-comparisons.cpp
@@ -1,13 +1,9 @@
 class Solution {
 public:
-    int dp[51][101][51];
-    int mod =1e9+7;
-    int fun(int n,int m,int k,int index,int maximum,int sc)
+    int dp[50][101][51];
+    int mod=1e9+7;
+    int solve(int n,int m,int k,int maximum,int sc,int index)
     {
-        if(sc>k)
-        {
-            return 0;
-        }
         if(index==n)
         {
             if(sc==k)
@@ -16,30 +12,30 @@ public:
             }
             return 0;
         }
-        if( maximum!=-1 && dp[index][maximum][sc]!=-1)
+        if(maximum!=-1 && dp[index][maximum][sc]!=-1)
         {
             return dp[index][maximum][sc];
         }
-        int result=0;
+          long long ans=0;
         for(int i=1;i<=m;i++)
         {
             if(i>maximum)
             {
-                result=(result+fun(n,m,k,index+1,i,sc+1)) % mod;
+                ans=(ans+solve(n,m,k,i,sc+1,index+1))%mod;
             }
             else
             {
-                result=(result+fun(n,m,k,index+1,maximum,sc)) % mod;
+                ans=(ans+solve(n,m,k,maximum,sc,index+1))%mod;
             }
         }
         if(maximum!=-1)
         {
-        return dp[index][maximum][sc]=result % mod;
+        return dp[index][maximum][sc]=ans;
         }
-        return result % mod;
+        return ans;
     }
     int numOfArrays(int n, int m, int k) {
         memset(dp,-1,sizeof(dp));
-        return fun(n,m,k,0,-1,0);
+        return solve(n,m,k,-1,0,0);
     }
 };
