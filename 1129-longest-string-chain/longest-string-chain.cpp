2f7abc27c17988385442a0841aaa981afc6866b3
SouravAgarwal69 <souravagarwal22072003@gmail.com>
2026-03-18 14:25:52 +0530

    Time: 39 ms (76.14%) | Memory: 18.5 MB (95.67%) - LeetSync

--- 1129-longest-string-chain/longest-string-chain.cpp
+++ 1129-longest-string-chain/longest-string-chain.cpp
@@ -1,52 +1,39 @@
 class Solution {
 public:
-    int dp[1000][1000];
-    bool isPossible(int prev,int index,vector<string>&words)
+    bool isPossible(int prev,int curr,vector<string>&words)
     {
-        if(words[prev].size()+1!=words[index].size())
+        if(words[prev].size()+1!=words[curr].size())
         {
-            return false;
+           return false;
         }
-        string s1=words[prev];
-        string s2=words[index];
         int i=0,j=0;
-        while(j<s2.size() && i<s1.size())
+        while(j<words[curr].size() && i<words[prev].size())
         {
-            if(s1[i]==s2[j])
+            if(words[curr][j]==words[prev][i])
             {
                 i++;
             }
             j++;
         }
-        return i==s1.size();
-    }
-    int findLength(vector<string>&words,int prev,int index)
-    {
-        if(index==words.size())
-        {
-            return 0;
-        }
-        if(prev!=-1 && dp[index][prev]!=-1)
-        {
-            return dp[index][prev];
-        }
-        int take=0;
-        if(prev==-1 || isPossible(prev,index,words))
-        {
-            take=1+findLength(words,index,index+1);
-        }
-        int skip=findLength(words,prev,index+1);
-        if(prev!=-1)
-        {
-           return dp[index][prev]=max(take,skip);
-        }
-        return max(take,skip);
+        return i==words[prev].size();
     }
     int longestStrChain(vector<string>& words) {
-        memset(dp,-1,sizeof(dp));
-        sort(words.begin(),words.end(),[](string &a,string &b){
-           return a.length()<b.length();
+        sort(words.begin(),words.end(),[](string &words1,string &words2){
+              return words1.size()<words2.size();
         });
-       return findLength(words,-1,0);
+        int ans=1;
+       vector<int>dp(words.size(),1);
+       for(int i=1;i<words.size();i++)
+       {
+          for(int j=0;j<i;j++)
+          {
+             if(isPossible(j,i,words))
+             {
+                dp[i]=max(dp[i],dp[j]+1);
+             }
+          }
+          ans=max(ans,dp[i]);
+       }
+       return ans;
     }
 };
