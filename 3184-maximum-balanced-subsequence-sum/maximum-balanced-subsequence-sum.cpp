7057a6b2228276dfd0a38d3a5817838397794c05
SouravAgarwal69 <souravagarwal22072003@gmail.com>
2026-03-18 07:25:22 +0530

    Time: 284 ms (47.85%) | Memory: 103 MB (26.72%) - LeetSync

--- 3184-maximum-balanced-subsequence-sum/maximum-balanced-subsequence-sum.cpp
+++ 3184-maximum-balanced-subsequence-sum/maximum-balanced-subsequence-sum.cpp
@@ -1,30 +1,25 @@
 class Solution {
 public:
     long long maxBalancedSubsequenceSum(vector<int>& nums) {
-        map<int,long long> mp;
-        long long result = LLONG_MIN;
-
-        for(int i = 0; i < nums.size(); i++) {
-            long long sum = nums[i];
-
-            auto it = mp.upper_bound(nums[i] - i);
-            if(it != mp.begin()) {
+        map<int,long long>mp;
+        long long result=LLONG_MIN;
+        for(int i=0;i<nums.size();i++)
+        {
+            long long sum=nums[i];
+            auto it=mp.upper_bound(nums[i]-i);
+            if(it!=mp.begin())
+            {
                 it--;
-                sum = max(it->second+nums[i],sum);
+                sum=max(sum,sum+it->second);
             }
-
-            result = max(result, sum);
-
-            // Remove dominated states
-            it = mp.upper_bound(nums[i] - i);
-            while(it != mp.end() && it->second <= sum) {
-                it = mp.erase(it);
-            }
-
-            // Insert current state
-            mp[nums[i] - i] = sum;
+           it=mp.upper_bound(nums[i]-i);
+           while(it!=mp.end() && it->second<sum)
+           {
+              it=mp.erase(it);
+           }
+           mp[nums[i]-i]=sum;
+           result=max(sum,result);
         }
-
         return result;
     }
 };
