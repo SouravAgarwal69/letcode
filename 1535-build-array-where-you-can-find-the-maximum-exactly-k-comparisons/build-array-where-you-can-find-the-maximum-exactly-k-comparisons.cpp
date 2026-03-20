91ef6f079b2ff038932009983614db06d8cc5899
SouravAgarwal69 <souravagarwal22072003@gmail.com>
2026-03-20 05:53:41 +0530

    Time: 123 ms (71.84%) | Memory: 8.8 MB (89.36%) - LeetSync

--- 1535-build-array-where-you-can-find-the-maximum-exactly-k-comparisons/build-array-where-you-can-find-the-maximum-exactly-k-comparisons.cpp
+++ 1535-build-array-where-you-can-find-the-maximum-exactly-k-comparisons/build-array-where-you-can-find-the-maximum-exactly-k-comparisons.cpp
@@ -1,41 +1,41 @@
 class Solution {
 public:
-    int dp[50][101][51];
     int mod=1e9+7;
-    int solve(int n,int m,int k,int maximum,int sc,int index)
+    int dp[50][51][101];
+    int find(int n,int m,int k,int index,int maximum,int sc)
     {
-        if(index==n)
+        if(sc>k)
         {
-            if(sc==k)
-            {
-                return 1;
-            }
             return 0;
         }
-        if(maximum!=-1 && dp[index][maximum][sc]!=-1)
-        {
-            return dp[index][maximum][sc];
-        }
-          long long ans=0;
-        for(int i=1;i<=m;i++)
-        {
-            if(i>maximum)
-            {
-                ans=(ans+solve(n,m,k,i,sc+1,index+1))%mod;
-            }
-            else
-            {
-                ans=(ans+solve(n,m,k,maximum,sc,index+1))%mod;
-            }
-        }
-        if(maximum!=-1)
-        {
-        return dp[index][maximum][sc]=ans;
-        }
-        return ans;
+          if(index==n)
+          {
+              if(sc==k)
+              {
+                return 1;
+              }
+              return 0;
+          }
+         int result=0;
+         if(dp[index][sc][maximum]!=-1)
+         {
+            return dp[index][sc][maximum];
+         }
+          for(int i=1;i<=m;i++)
+          {
+              if(i>maximum)
+              {
+                result=(result+find(n,m,k,index+1,i,sc+1))%mod;
+              }
+              else
+              {
+                  result=(result+find(n,m,k,index+1,maximum,sc))%mod;
+              }
+          }
+          return dp[index][sc][maximum]=result%mod;
     }
     int numOfArrays(int n, int m, int k) {
         memset(dp,-1,sizeof(dp));
-        return solve(n,m,k,-1,0,0);
+       return find(n,m,k,0,0,0)%mod;
     }
 };
