494d44fdba98683de1660838eafa8ce75111c323
SouravAgarwal69 <souravagarwal22072003@gmail.com>
2026-03-25 19:16:22 +0530

    Time: 17 ms (38.22%) | Memory: 41.8 MB (42.40%) - LeetSync

--- 853-most-profit-assigning-work/most-profit-assigning-work.cpp
+++ 853-most-profit-assigning-work/most-profit-assigning-work.cpp
@@ -1,26 +1,35 @@
 class Solution {
 public:
     int maxProfitAssignment(vector<int>& difficulty, vector<int>& profit, vector<int>& worker) {
-        int n=difficulty.size();
         vector<pair<int,int>>vp;
         for(int i=0;i<difficulty.size();i++)
         {
             vp.push_back(make_pair(difficulty[i],profit[i]));
         }
         sort(vp.begin(),vp.end());
-        sort(worker.begin(),worker.end());
-        int maxProfit=0;
-        long long total=0;
-        int i=0,j=0;
-        while(j<worker.size() )
+        for(int i=1;i<vp.size();i++)
         {
-           while(i<n && worker[j]>=vp[i].first)  
-           {
-               maxProfit=max(vp[i].second,maxProfit);
-               i++;
-           }
-              total+=maxProfit;
-              j++;          
+            vp[i].second=max(vp[i-1].second,vp[i].second);
+        }
+        int total=0;
+        for(int i=0;i<worker.size();i++)
+        {
+            int s=0,e=difficulty.size()-1;
+            int ans=0;
+            while(s<=e)
+            {
+                int mid=s+(e-s)/2;
+                if(worker[i]>=vp[mid].first)
+                {
+                    ans=vp[mid].second;
+                    s=mid+1;
+                }
+                else
+                {
+                    e=mid-1;
+                }
+            }
+            total+=ans;
         }
         return total;
     }
