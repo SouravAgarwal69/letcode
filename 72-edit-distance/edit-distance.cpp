3e1c45b468d1309e2ebb5ed60ea0757aaf77edfc
SouravAgarwal69 <souravagarwal22072003@gmail.com>
2026-03-27 05:42:58 +0530

    Time: 51 ms (12.96%) | Memory: 11.9 MB (85.79%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -1,31 +1,27 @@
 class Solution {
 public:
     int dp[501][501];
-    int operation (string &word1,string &word2,int i,int j)
+    int findminDistance(string &word1,string &word2,int m,int n)
     {
-        if(i==word1.size())
-        {
-            return word2.size()-j;
-        }
-        if(j==word2.size())
-        {
-            return word1.size()-i;
-        }
-        if(word1[i]==word2[j])
-        {
-            return operation(word1,word2,i+1,j+1);
-        }
-        if(dp[i][j]!=-1)
-        {
-            return dp[i][j];
-        }
-        int insert=1+operation(word1,word2,i,j+1);
-        int Delete=1+operation(word1,word2,i+1,j);
-        int replace=1+operation(word1,word2,i+1,j+1);
-        return  dp[i][j]=min({insert,Delete,replace});
+         if(m==0 || n==0)
+         {
+            return m+n;
+         }
+           if(dp[m][n]!=-1)
+         {
+            return dp[m][n];
+         }
+         if(word1[m-1]==word2[n-1])
+         {
+            return dp[m][n]=findminDistance(word1,word2,m-1,n-1);
+         }
+         int insert=1+findminDistance(word1,word2,m,n-1);
+         int Delete=1+findminDistance(word1,word2,m-1,n);
+         int Replace=1+findminDistance(word1,word2,m-1,n-1);
+         return dp[m][n]=min({insert,Delete,Replace});
     }
     int minDistance(string word1, string word2) {
         memset(dp,-1,sizeof(dp));
-        return operation(word1,word2,0,0);
+        return findminDistance(word1,word2,word1.size(),word2.size());
     }
 };
