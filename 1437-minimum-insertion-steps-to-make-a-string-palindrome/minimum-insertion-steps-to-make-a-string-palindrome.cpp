7881e8584f4925f99b96fddc3a0c38979bb6a4d7
SouravAgarwal69 <souravagarwal22072003@gmail.com>
2026-03-29 05:33:18 +0530

    Time: 23 ms (79.34%) | Memory: 31.6 MB (39.01%) - LeetSync

--- 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.cpp
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -1,27 +1,22 @@
 class Solution {
-    public:
-    int dp[500][500];
-    int find(string &s,int i,int j)
-    {
-        if(i>=j)
-        {
-            return 0;
-        }
-        if(dp[i][j]!=-1)
-        {
-            return dp[i][j];
-        }
-        if(s[i]==s[j])
-        {
-            return dp[i][j]=find(s,i+1,j-1);
-        }
-        else
-        {
-            return dp[i][j]=1+min(find(s,i+1,j),find(s,i,j-1));
-        }
-    }
+public:
     int minInsertions(string s) {
-        memset(dp,-1,sizeof(dp));
-        return find(s,0,s.size()-1);
+        vector<vector<int>>dp(s.size(),vector<int>(s.size()));
+        for(int L=2;L<=s.size();L++)
+        {
+           for(int i=0;i+L-1<s.size();i++)
+           {
+                 int j=i+L-1;
+                 if(s[i]==s[j])
+                 {
+                    dp[i][j]=dp[i+1][j-1];
+                 }
+                 else
+                 {
+                    dp[i][j]=1+min(dp[i+1][j],dp[i][j-1]);
+                 }
+           }
+        }
+        return dp[0][s.size()-1];
     }
 };
