afdf4af59637bc6dde8bf0851e35096a2e873ee2
SouravAgarwal69 <souravagarwal22072003@gmail.com>
2026-04-04 06:14:44 +0530

    Time: 0 ms (100.00%) | Memory: 11.3 MB (19.66%) - LeetSync

--- 24-swap-nodes-in-pairs/swap-nodes-in-pairs.cpp
+++ 24-swap-nodes-in-pairs/swap-nodes-in-pairs.cpp
@@ -15,8 +15,7 @@ public:
         {
             return head;
         }
-        ListNode*first=head;
-        ListNode*second=head->next;
+        ListNode*first=head,*second=head->next;
         first->next=swapPairs(second->next);
         second->next=first;
         return second;
