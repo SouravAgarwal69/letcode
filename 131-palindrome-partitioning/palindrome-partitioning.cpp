9e3d6aef4fd01741e1061ae63c5f64e6d9733b41
SouravAgarwal69 <souravagarwal22072003@gmail.com>
2026-04-07 12:44:44 +0530

    Time: 34 ms (72.85%) | Memory: 53.5 MB (81.94%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.cpp
+++ 131-palindrome-partitioning/palindrome-partitioning.cpp
@@ -1,26 +1,26 @@
 class Solution {
 public:
-    void fun(string &s,vector<vector<string>>&result,vector<string>&ans,vector<vector<bool>>&mat,int index)
+    void fun(string &s,vector<string>&temp,vector<vector<string>>&result,int index,vector<vector<bool>>&mat)
     {
         if(index==s.size())
         {
-            result.push_back(ans);
+            result.push_back(temp);
             return;
         }
         for(int i=index;i<s.size();i++)
         {
-            string temp=s.substr(index,i-index+1);
+            string st=s.substr(index,i-index+1);
             if(mat[index][i])
             {
-                ans.push_back(temp);
-                fun(s,result,ans,mat,i+1);
-                ans.pop_back();
+            temp.push_back(st);
+            fun(s,temp,result,i+1,mat);
+            temp.pop_back();
             }
         }
     }
     vector<vector<string>> partition(string s) {
         vector<vector<string>>result;
-        vector<string>ans;
+        vector<string>temp;
         vector<vector<bool>>mat(s.size(),vector<bool>(s.size()));
         for(int L=1;L<=s.size();L++)
         {
@@ -36,12 +36,12 @@ public:
                     mat[i][j]=(s[i]==s[j]);
                 }
                 else
-                {
+                 {
                     mat[i][j]=(s[i]==s[j] && mat[i+1][j-1]);
-                }
+                 }
             }
         }
-        fun(s,result,ans,mat,0);
+        fun(s,temp,result,0,mat);
         return result;
     }
 };
