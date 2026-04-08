1ec86d865390e26d8cb8b623891a7a9439529dfd
SouravAgarwal69 <souravagarwal22072003@gmail.com>
2026-04-08 18:05:51 +0530

    Time: 0 ms (100.00%) | Memory: 9.5 MB (28.32%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -4,20 +4,17 @@ public:
         vector<vector<int>>mat(m,vector<int>(n));
         for(int i=0;i<m;i++)
         {
-            for(int j=0;j<n;j++)
+            mat[i][0]=1;
+        }
+        for(int j=0;j<n;j++)
+        {
+            mat[0][j]=1;
+        }
+        for(int i=1;i<m;i++)
+        {
+            for(int j=1;j<n;j++)
             {
-                if(i==0)
-                {
-                    mat[i][j]=1;
-                }
-                else if(j==0)
-                {
-                    mat[i][j]=1;
-                }
-                else
-                {
-                    mat[i][j]=mat[i-1][j]+mat[i][j-1];
-                }
+                mat[i][j]=mat[i][j-1]+mat[i-1][j];
             }
         }
         return mat[m-1][n-1];
