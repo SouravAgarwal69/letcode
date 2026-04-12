795e89968eac2e2c9e4d616e521e84e2e1f3df56
SouravAgarwal69 <souravagarwal22072003@gmail.com>
2026-04-12 06:25:47 +0530

    Time: 63 ms (63.41%) | Memory: 156.2 MB (54.70%) - LeetSync

--- 3648-find-the-maximum-number-of-fruits-collected/find-the-maximum-number-of-fruits-collected.cpp
+++ 3648-find-the-maximum-number-of-fruits-collected/find-the-maximum-number-of-fruits-collected.cpp
@@ -1,54 +1,48 @@
 class Solution {
 public:
-    int find1(vector<vector<int>>&fruits)
-    {
-        int total=0;
-        for(int i=0;i<fruits.size();i++)
-        {
-            total+=fruits[i][i];
-            fruits[i][i]=0;
-        }
-       return total;
-    }
-    int find2(vector<vector<int>>&fruits,int i,int j,vector<vector<int>>&dp)
-    {
-        int n=fruits.size();
-        if(i==n-1 || j==n || i>j)
-        {
-            return 0;
-        }
-        if(dp[i][j]!=-1)
-        {
-            return dp[i][j];
-        }
-        int path1=find2(fruits,i+1,j-1,dp);
-        int path2=find2(fruits,i+1,j,dp);
-        int path3=find2(fruits,i+1,j+1,dp);
-        return dp[i][j]=fruits[i][j]+max({path1,path2,path3});
-    }
-    int find3(vector<vector<int>>&fruits,int i,int j,vector<vector<int>>&dp)
-    {
-        int n=fruits.size();
-        if(j==n-1 || i<j || i==n)
-        {
-            return 0;
-        }
-        if(dp[i][j]!=-1)
-        {
-            return dp[i][j];
-        }
-        int path1=find3(fruits,i-1,j+1,dp);
-        int path2=find3(fruits,i,j+1,dp);
-        int path3=find3(fruits,i+1,j+1,dp);
-        return dp[i][j]=fruits[i][j]+max({path1,path2,path3});
-    }
     int maxCollectedFruits(vector<vector<int>>& fruits) {
-        int n=fruits.size();
-        int child1=find1(fruits);
-        vector<vector<int>>dp1(n,vector<int>(n,-1));
-        vector<vector<int>>dp2(n,vector<int>(n,-1));
-        int child2=find2(fruits,0,n-1,dp1);
-        int child3=find3(fruits,n-1,0,dp2);
-        return child1+child2+child3;
+        int n = fruits.size();
+        vector<vector<int>> t(n, vector<int>(n, 0));
+        //t[i][j] = max fruits collected till [i][j]
+
+        //child1Collect - Diagonal elements
+        int result = 0;
+        for(int i = 0; i < n; i++) {
+            result += fruits[i][i];
+        }
+
+
+        //Before child2 and child3, nullify the cells which can't be visited by child2 and child3
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                if(i < j && i+j < n-1) {
+                    t[i][j] = 0;
+                } else if(i > j && i+j < n-1) {
+                    t[i][j] = 0;
+                } else {
+                    t[i][j] = fruits[i][j];
+                }
+            }
+        }
+
+
+        //child2 collect fruits
+        //cells upper to diagonal : i < j
+        for(int i = 1; i < n; i++) {
+            for(int j = i+1; j < n; j++) {
+                t[i][j] += max({t[i-1][j-1], t[i-1][j], (j+1 < n) ? t[i-1][j+1] : 0});
+            }
+        }
+
+        //child3 collect fruits
+        //cells upper to diagonal : i > j
+        for(int j = 1; j < n; j++) {
+            for(int i = j+1; i < n; i++) {
+                t[i][j] += max({t[i-1][j-1], t[i][j-1], (i+1 < n) ? t[i+1][j-1] : 0});
+            }
+        }
+
+        return result + t[n-2][n-1] + t[n-1][n-2];
+
     }
 };
