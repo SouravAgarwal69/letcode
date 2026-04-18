3b9539724f6cd3e4594297245c234240469c406b
SouravAgarwal69 <souravagarwal22072003@gmail.com>
2026-04-18 17:45:29 +0530

    Time: 40 ms (90.67%) | Memory: 29.2 MB (28.68%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -1,41 +1,41 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        vector<vector<int>>ans;
+        vector<vector<int>>result;
         sort(nums.begin(),nums.end());
-        for(int i=0;i<nums.size();i++)
+        for(int i=0;i<nums.size()-2;i++)
         {
             if(i>0 && nums[i]==nums[i-1])
             {
                 continue;
             }
-            int s=i+1,e=nums.size()-1;
+            int target=-nums[i],s=i+1,e=nums.size()-1;
             while(s<e)
             {
-                if( nums[s]+nums[e]==-nums[i])
+                if(nums[s]+nums[e]==target)
                 {
-                 while(s<e && nums[s]==nums[s+1])
+                    while(s<e && nums[s]==nums[s+1])
+                    {
+                        s++;
+                    }
+                    while(s<e && nums[e]==nums[e-1])
+                    {
+                        e--;
+                    }
+                    result.push_back({nums[i],nums[s],nums[e]});
+                    s++;
+                    e--;
+                }
+                else if(nums[s]+nums[e]<target)
                 {
                     s++;
-                }
-                while(s<e && nums[e]==nums[e-1])
-                {
-                    e--;
-                }
-                    ans.push_back({nums[i],nums[s],nums[e]});
-                    s++;
-                    e--;
-                }
-                else if(nums[s]+nums[e]>-nums[i])
-                {
-                    e--;
                 }
                 else
                 {
-                    s++;
+                    e--;
                 }
-            }  
+            }
         }
-        return ans;
+        return result;
     }
 };
