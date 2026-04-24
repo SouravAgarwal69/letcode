94dfee12ac124f76297635fe9b8d81eb5f4f71ac
SouravAgarwal69 <souravagarwal22072003@gmail.com>
2026-04-24 13:35:34 +0530

    Time: 0 ms (100.00%) | Memory: 25.1 MB (88.78%) - LeetSync

--- 645-set-mismatch/set-mismatch.cpp
+++ 645-set-mismatch/set-mismatch.cpp
@@ -1,21 +1,23 @@
 class Solution {
 public:
     vector<int> findErrorNums(vector<int>& nums) {
-        int missing ,duplicate;
-        unordered_map<int,int>mp;
+        int missing=-1,duplicate=-1;
         for(int i=0;i<nums.size();i++)
         {
-            mp[nums[i]]++;
-        }
-        for(int i=1;i<=nums.size();i++)
-        {
-            if(mp[i]==2)
+            if(nums[abs(nums[i])-1]<0)
             {
-               duplicate=i ;
+                duplicate=abs(nums[i]);
             }
-            if(mp[i]==0)
+            else
             {
-              missing=i;
+                nums[abs(nums[i])-1]*=-1;
+            }
+        }
+        for(int i=0;i<nums.size();i++)
+        {
+            if(nums[i]>0)
+            {
+                missing=i+1;
             }
         }
         return {duplicate,missing};
