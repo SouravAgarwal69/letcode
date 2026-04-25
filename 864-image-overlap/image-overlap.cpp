4bdc1d570454a74ae5c0c9311becc55bf4baa407
SouravAgarwal69 <souravagarwal22072003@gmail.com>
2026-04-25 09:36:48 +0530

    Time: 49 ms (61.29%) | Memory: 12.9 MB (74.55%) - LeetSync

--- 864-image-overlap/image-overlap.cpp
+++ 864-image-overlap/image-overlap.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
-    int countOverlap(vector<vector<int>>&img1,vector<vector<int>>&img2,int row_offSet,int col_offSet,int n)
+    int countOverlap(vector<vector<int>>&img1,vector<vector<int>>&img2,int row,int col,int n)
     {
         int overlap=0;
-        for(int i=0;i<img1.size();i++)
+        for(int i=0;i<n;i++)
         {
-            for(int j=0;j<img2.size();j++)
+            for(int j=0;j<n;j++)
             {
-                if(i+row_offSet<0 || i+row_offSet>=n || j+col_offSet<0 || j+col_offSet>=n)
+                if(i+row>=n || i+row<0 || j+col>=n || j+col<0)
                 {
                     continue;
                 }
-                if(img1[i][j]==1 && img2[i+row_offSet][j+col_offSet]==1)
+                if(img1[i][j]==1 && img2[i+row][j+col]==1)
                 {
                      overlap++;
                 }
@@ -20,16 +20,15 @@ public:
         return overlap;
     }
     int largestOverlap(vector<vector<int>>& img1, vector<vector<int>>& img2) {
-        int n=img1.size();
-        int maxOverlaps=0;
-        for(int row_offSet=-n+1;row_offSet<n;row_offSet++)
+        int maxOverlap=0,n=img1.size();
+        for(int row_shift=-n+1;row_shift<n;row_shift++)
         {
-            for(int col_offSet=-n+1;col_offSet<n;col_offSet++)
+            for(int col_shift=-n+1;col_shift<n;col_shift++)
             {
-              int  overlap=countOverlap(img1,img2,row_offSet,col_offSet,n);
-                maxOverlaps=max(maxOverlaps,overlap);
+                int overlap=countOverlap(img1,img2,row_shift,col_shift,n);
+                maxOverlap=max(maxOverlap,overlap);
             }
         }
-        return maxOverlaps;
+        return maxOverlap;
     }
 };
