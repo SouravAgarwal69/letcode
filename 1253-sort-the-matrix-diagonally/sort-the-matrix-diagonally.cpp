3dfaf6fc54de68b757cd64cc0ec023396d025d87
SouravAgarwal69 <souravagarwal22072003@gmail.com>
2026-04-27 23:21:30 +0530

    Time: 3 ms (57.78%) | Memory: 13.8 MB (22.88%) - LeetSync

--- 1253-sort-the-matrix-diagonally/sort-the-matrix-diagonally.cpp
+++ 1253-sort-the-matrix-diagonally/sort-the-matrix-diagonally.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     vector<vector<int>> diagonalSort(vector<vector<int>>& mat) {
-        unordered_map<int,vector<int>>mp;
+        map<int,vector<int>>mp;
         for(int i=0;i<mat.size();i++)
         {
             for(int j=0;j<mat[0].size();j++)
             {
-                mp[i-j].push_back(mat[i][j]);
+                mp[j-i].push_back(mat[i][j]);
             }
         }
         for(auto it=mp.begin();it!=mp.end();it++)
@@ -17,8 +17,8 @@ public:
         {
             for(int j=mat[0].size()-1;j>=0;j--)
             {
-                mat[i][j]=mp[i-j].back();
-                mp[i-j].pop_back();
+                mat[i][j]=mp[j-i].back();
+                mp[j-i].pop_back();
             }
         }
         return mat;
