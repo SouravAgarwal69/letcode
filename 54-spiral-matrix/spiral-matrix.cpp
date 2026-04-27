ccfc2cdca0e17f7990e62c7fbf9f5e2590fb7d16
SouravAgarwal69 <souravagarwal22072003@gmail.com>
2026-04-27 13:57:15 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (98.00%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.cpp
+++ 54-spiral-matrix/spiral-matrix.cpp
@@ -17,19 +17,19 @@ public:
             right--;
             if(top<=bottom)
             {
-            for(int i=right;i>=left;i--)
-            {
-                result.push_back(matrix[bottom][i]);
-            }
-            bottom--;
+                for(int i=right;i>=left;i--)
+                {
+                    result.push_back(matrix[bottom][i]);
+                }
+                bottom--;
             }
             if(left<=right)
             {
-            for(int i=bottom;i>=top;i--)
-            {
-                result.push_back(matrix[i][left]);
-            }
-            left++;
+                for(int i=bottom;i>=top;i--)
+                {
+                    result.push_back(matrix[i][left]);
+                }
+                left++;
             }
         }
         return result;
