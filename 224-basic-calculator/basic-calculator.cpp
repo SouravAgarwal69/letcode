b0537ba3fba0360d67f101aac2a01b9ab5abfd68
SouravAgarwal69 <souravagarwal22072003@gmail.com>
2026-04-29 14:23:48 +0530

    Time: 4 ms (41.48%) | Memory: 13.6 MB (43.78%) - LeetSync

--- 224-basic-calculator/basic-calculator.cpp
+++ 224-basic-calculator/basic-calculator.cpp
@@ -2,16 +2,17 @@ class Solution {
 public:
     int calculate(string s) {
         stack<int>st;
-        long long res=0,num=0,sign=1;
+        long long num=0,res=0,sign=1;
         for(int i=0;i<s.size();i++)
         {
             if(s[i]=='(')
             {
+                res+=num*sign;
                 st.push(res);
                 st.push(sign);
                 res=0;
-                num=0;
                 sign=1;
+                num=0;
             }
             else if(s[i]=='+' || s[i]=='-')
             {
@@ -25,17 +26,16 @@ public:
             }
             else if(s[i]==')')
             {
-                res+=num*sign;
-                int prevSign=st.top();
-                st.pop();
-                int prevVal=st.top();
-                st.pop();
-                res=prevSign*res+prevVal;
-                num=0;
-                sign=1;
+               res+=num*sign;
+               int prevSign=st.top();
+               st.pop();
+               int prevVal=st.top();
+               st.pop();
+               res=res*prevSign+prevVal;
+               num=0;
             }
         }
-        res=res+num*sign;
+        res+=num*sign;
         return res;
     }
 };
