8b39602ed70b72417f23502d00c84eae4c5663e7
SouravAgarwal69 <souravagarwal22072003@gmail.com>
2026-05-02 10:03:27 +0530

    Time: 0 ms (100.00%) | Memory: 13.4 MB (63.64%) - LeetSync

--- 137-single-number-ii/single-number-ii.cpp
+++ 137-single-number-ii/single-number-ii.cpp
@@ -4,24 +4,20 @@ public:
         int result=0;
         for(int k=0;k<32;k++)
         {
-           int temp=1<<k;
-           int cntZero=0,cntOne=0;
-        for(int i=0;i<nums.size();i++)
-        {
-            int bit=nums[i] & temp;
-            if(bit==0)
+            int temp=1<<k;
+            int cntOne=0;
+            for(int i=0;i<nums.size();i++)
             {
-                cntZero++;
+                int bit=(temp & nums[i]);
+                if(bit!=0)
+                {
+                    cntOne++;
+                }
             }
-          else
+            if(cntOne % 3!=0)
             {
-                cntOne++;
+                result=result | temp;
             }
-        }
-          if(cntOne%3!=0)
-          {
-             result=result | temp;
-          }
         }
         return result;
     }
