eaccbc806c685689b9bd934db39d1898027b0d15
SouravAgarwal69 <souravagarwal22072003@gmail.com>
2026-05-08 06:23:36 +0530

    Time: 0 ms (100.00%) | Memory: 21.7 MB (69.78%) - LeetSync

--- 735-asteroid-collision/asteroid-collision.cpp
+++ 735-asteroid-collision/asteroid-collision.cpp
@@ -7,22 +7,22 @@ public:
             bool collision=false;
             while(!result.empty() && asteroids[i]<0 && result.back()>0)
             {
-                if(abs(asteroids[i])==result.back())
+                if(result.back()==abs(asteroids[i]))
                 {
                     result.pop_back();
                     collision=true;
                     break;
                 }
-                if(abs(asteroids[i])>result.back())
+                else if(result.back()<abs(asteroids[i]))
                 {
-                   result.pop_back();
+                    result.pop_back();
                 }
                 else
                 {
                     collision=true;
                     break;
                 }
-            } 
+            }
             if(!collision)
             {
                 result.push_back(asteroids[i]);
