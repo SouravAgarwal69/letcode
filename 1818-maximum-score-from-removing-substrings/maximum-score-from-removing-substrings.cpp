53b7e77195a350c4848d91c5b60dd370a9ab6cb8
SouravAgarwal69 <souravagarwal22072003@gmail.com>
2026-05-09 11:27:49 +0530

    Time: 45 ms (52.86%) | Memory: 27.4 MB (56.25%) - LeetSync

--- 1818-maximum-score-from-removing-substrings/maximum-score-from-removing-substrings.cpp
+++ 1818-maximum-score-from-removing-substrings/maximum-score-from-removing-substrings.cpp
@@ -1,37 +1,37 @@
 class Solution {
 public:
     int maximumGain(string s, int x, int y) {
-        int totalGain=0;
-        string maxString=x>y?"ab":"ba";
-        string minString=x>y?"ba":"ab";
-        int maxScore=max(x,y);
-        int minScore=min(x,y);
+        int total=0;
+        string target=x>y?"ab":"ba";
+        int val=x>y?x:y;
         string result;
         for(int i=0;i<s.size();i++)
         {
-            if(!result.empty() && result.back()==maxString[0] && s[i]==maxString[1])
+            if(!result.empty() && result.back()==target[0] && s[i]==target[1])
             {
                 result.pop_back();
-                totalGain+=maxScore;
+                total+=val;
             }
             else
             {
-               result.push_back(s[i]);   
+                result.push_back(s[i]);
             }
         }
+        target=x>y?"ba":"ab";
+        val=min(x,y);
         string temp;
         for(int i=0;i<result.size();i++)
         {
-            if(!temp.empty() && temp.back()==minString[0] && result[i]==minString[1])
+            if(!temp.empty() && temp.back()==target[0] && result[i]==target[1])
             {
                 temp.pop_back();
-                totalGain+=minScore;
+                total+=val;
             }
             else
             {
                 temp.push_back(result[i]);
             }
         }
-        return totalGain;
+        return total;
     }
 };
