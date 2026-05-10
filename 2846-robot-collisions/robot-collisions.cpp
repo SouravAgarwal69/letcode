99c82f6ec9b425bec5b4065b9e07e3b7b4ecafba
SouravAgarwal69 <souravagarwal22072003@gmail.com>
2026-05-10 06:22:17 +0530

    Time: 43 ms (92.34%) | Memory: 196.6 MB (49.78%) - LeetSync

--- 2846-robot-collisions/robot-collisions.cpp
+++ 2846-robot-collisions/robot-collisions.cpp
@@ -1,52 +1,54 @@
 class Solution {
 public:
     vector<int> survivedRobotsHealths(vector<int>& positions, vector<int>& healths, string directions) {
-        vector<int>indices(positions.size());
-        iota(indices.begin(),indices.end(),0);
-        sort(indices.begin(),indices.end(),[&](int i,int j){
+        vector<int>result;
+        vector<int>temp;
+        int n=positions.size();
+        vector<int>index(n);
+        iota(index.begin(),index.end(),0);
+        sort(index.begin(),index.end(),[&](int i,int j)
+        {
             return positions[i]<positions[j];
         });
-        vector<int>result;
-        stack<int>st;
-        for(int i=0;i<indices.size();i++)
+        for(int i=0;i<n;i++)
         {
             bool collision=false;
-            while(!st.empty() && directions[indices[i]]=='L' && directions[st.top()]=='R')
+            while(!result.empty() && directions[result.back()]=='R' && directions[index[i]]=='L')
             {
-                if(healths[indices[i]]==healths[st.top()])
+                if(healths[result.back()]==healths[index[i]])
                 {
-                    healths[indices[i]]=0;
-                    healths[st.top()]=0;
-                    st.pop();
+                    healths[result.back()]=0;
+                    healths[index[i]]=0;
+                    result.pop_back();
                     collision=true;
                     break;
                 }
-                else if(healths[indices[i]]>healths[st.top()])
+                else if(healths[result.back()]>healths[index[i]])
                 {
-                    healths[st.top()]=0;
-                    healths[indices[i]]-=1;
-                    st.pop();
+                    healths[index[i]]=0;
+                    healths[result.back()]--;
+                    collision=true;
+                    break;
                 }
                 else
                 {
-                    healths[indices[i]]=0;
-                    healths[st.top()]-=1;
-                    collision=true;
-                    break;
+                    healths[index[i]]--;
+                    healths[result.back()]=0;
+                    result.pop_back();
                 }
             }
             if(!collision)
             {
-                st.push(indices[i]);
+                result.push_back(index[i]);
             }
         }
-         for(int i=0;i<healths.size();i++)
-         {
-            if(healths[i]!=0)
-            {
-                result.push_back(healths[i]);
-            }
-         }
-         return result;
+        for(int i=0;i<n;i++)
+        {
+             if(healths[i]!=0)
+             {
+                temp.push_back(healths[i]);
+             }
+        }
+        return temp;
     }
 };
