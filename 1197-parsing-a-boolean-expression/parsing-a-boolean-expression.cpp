bdf78b0487b04c17e2793c59dd94e9b650883625
SouravAgarwal69 <souravagarwal22072003@gmail.com>
2026-05-12 11:47:34 +0530

    Time: 4 ms (46.32%) | Memory: 9.9 MB (82.23%) - LeetSync

--- 1197-parsing-a-boolean-expression/parsing-a-boolean-expression.cpp
+++ 1197-parsing-a-boolean-expression/parsing-a-boolean-expression.cpp
@@ -1,60 +1,61 @@
 class Solution {
 public:
-    char find(char op,string &result)
+    char find(char op,string temp)
     {
-        if(op=='&')
+        if(op=='|')
         {
-            for(int i=0;i<result.size();i++)
+            for(int i=0;i<temp.size();i++)
             {
-                if(result[i]=='f')
+                if(temp[i]=='t')
+                {
+                    return 't';
+                }
+            }
+           return 'f';
+        }
+        else if(op=='&')
+        {
+            for(int j=0;j<temp.size();j++)
+            {
+                if(temp[j]=='f')
                 {
                     return 'f';
                 }
             }
             return 't';
         }
-        if(op=='|')
-        {
-            for(int i=0;i<result.size();i++)
-            {
-                if(result[i]=='t')
-                {
-                    return 't';
-                }
-            }
-            return 'f';
-        }
         else
         {
-            if(result.back()=='t')
-            {
-                return 'f';
-            }
-            return 't';
+           return temp.back()=='t'?'f':'t';
         }
     }
     bool parseBoolExpr(string expression) {
-        string result;
-        for(int i=0;i<expression.size();i++)
-        {
-            if(!result.empty() && expression[i]==')')
+            stack<char>st;
+            for(int i=0;i<expression.size();i++)
             {
-                string temp;
-                while(result.back()!='(')
+                if(expression[i]==',')
                 {
-                    temp.push_back(result.back());
-                    result.pop_back();
+                   continue;
                 }
-                result.pop_back();
-                char op=result.back();
-                result.pop_back();
-                result.push_back(find(op,temp));
+                else if(expression[i]==')')
+                {
+                    string temp;
+                    while(!st.empty() && st.top()!='(')
+                    {
+                       temp.push_back(st.top());
+                       st.pop();
+                    }
+                    st.pop();
+                    char op=st.top();
+                    st.pop();
+                    char ch=find(op,temp);
+                    st.push(ch);
+                }
+                else
+                {
+                    st.push(expression[i]);
+                }
             }
-            else
-            {
-                result.push_back(expression[i]);
-            }
-        }
-        return result.back()=='t'?true:false;
+            return st.top()=='t'?true:false;
     }
 };
