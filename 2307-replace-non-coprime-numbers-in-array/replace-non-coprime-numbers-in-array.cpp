fc3afbf663a9f30e97827f647243c4d0caf3fc8e
SouravAgarwal69 <souravagarwal22072003@gmail.com>
2026-05-18 11:36:15 +0530

    Time: 13 ms (83.82%) | Memory: 125 MB (93.02%) - LeetSync

--- 2307-replace-non-coprime-numbers-in-array/replace-non-coprime-numbers-in-array.cpp
+++ 2307-replace-non-coprime-numbers-in-array/replace-non-coprime-numbers-in-array.cpp
@@ -4,21 +4,20 @@ public:
         vector<int>result;
         for(int i=0;i<nums.size();i++)
         {
-           int first=nums[i];
-           while(!result.empty())
-           {
-                 int second=result.back();
-                int GCD=__gcd(first,second);
-                if(GCD==1)
+            int first=nums[i];
+            while(!result.empty())
+            {
+                int second=result.back();
+                int gcd=__gcd(first,second);
+                if(gcd==1)
                 {
-                   break;
+                    break;
                 }
-                int LCM=(1LL*first*second)/GCD;
+                int lcm=(1LL*first*second)/gcd;
+                first=lcm;
                 result.pop_back();
-                first=LCM;
-           }
-        result.push_back(first);        
-        
+            }
+            result.push_back(first);
         }
         return result;
     }
