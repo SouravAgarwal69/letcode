bc00b1a3a8b67b9669900db121d7a43fd02679bc
SouravAgarwal69 <souravagarwal22072003@gmail.com>
2026-05-21 12:32:41 +0530

    Time: 11 ms (64.42%) | Memory: 70.6 MB (70.94%) - LeetSync

--- 456-132-pattern/132-pattern.cpp
+++ 456-132-pattern/132-pattern.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
     bool find132pattern(vector<int>& nums) {
-        stack<int>num3;
-        int num2=INT_MIN;
+        stack<int>st;
+        int second=INT_MIN;
         for(int i=nums.size()-1;i>=0;i--)
         {
-            if(nums[i]<num2)
+            if(nums[i]<second)
             {
                 return true;
             }
-            while(!num3.empty() && num3.top()<nums[i])
+            while(!st.empty() && st.top()<nums[i])
             {
-                num2=num3.top();
-                num3.pop();
+                second=st.top();
+                st.pop();
             }
-            num3.push(nums[i]);
+            st.push(nums[i]);
         }
         return false;
     }
