7c307b5e7beced29ad7e2175e75e602da8769c82
SouravAgarwal69 <souravagarwal22072003@gmail.com>
2026-05-22 23:16:52 +0530

    Time: 0 ms (100.00%) | Memory: 10.3 MB (53.42%) - LeetSync

--- 726-number-of-atoms/number-of-atoms.cpp
+++ 726-number-of-atoms/number-of-atoms.cpp
@@ -5,8 +5,8 @@ public:
         stack<unordered_map<string,int>>st;
         st.push(unordered_map<string,int>());
         int i=0;
-         while(i<formula.size())
-         {
+        while(i<formula.size())
+        {
             if(formula[i]=='(')
             {
                 st.push(unordered_map<string,int>());
@@ -16,7 +16,7 @@ public:
             {
                 unordered_map<string,int>curr=st.top();
                 st.pop();
-                i++;
+                 i++;
                 string digit;
                 while(i<formula.size() && isdigit(formula[i]))
                 {
@@ -26,44 +26,45 @@ public:
                 if(!digit.empty())
                 {
                     int val=stoi(digit);
-                   for(auto it=curr.begin();it!=curr.end();it++)
-                   {
-                      it->second=it->second*val;
-                   }
+                    for(auto it=curr.begin();it!=curr.end();it++)
+                    {
+                        it->second=it->second*val;
+                    }
                 }
                 for(auto it=curr.begin();it!=curr.end();it++)
                 {
                     st.top()[it->first]+=it->second;
                 }
             }
-        else{
-                  string temp;
-                  temp+=formula[i];
-                  i++;
-                  while(i<formula.size() && formula[i]>='a' && formula[i]<='z')
-                  {
-                      temp+=formula[i];
-                      i++;
-                  }
-                  string digit;
+            else
+            {
+                string temp;
+                string digit;
+                temp+=formula[i];
+                i++;
+                 while(i<formula.size() && formula[i]>='a' && formula[i]<='z')
+                 {
+                    temp+=formula[i];
+                    i++;
+                 }
                  while(i<formula.size() && isdigit(formula[i]))
                  {
                     digit+=formula[i];
                     i++;
                  }
-               int val=digit.empty()?1:stoi(digit);
-               st.top()[temp]+=val;
+                 int val=digit.empty()?1:stoi(digit);
+                 st.top()[temp]+=val;
             }
-         }
-         map<string,int>mp(st.top().begin(),st.top().end());
-         for(auto it=mp.begin();it!=mp.end();it++)
-         {
+        }
+        map<string,int>mp(st.top().begin(),st.top().end());
+        for(auto it=mp.begin();it!=mp.end();it++)
+        {
             result+=it->first;
             if(it->second>1)
             {
-            result+=to_string(it->second);
+                result+=to_string(it->second);
             }
-         }
+        }
         return result;
     }
 };
