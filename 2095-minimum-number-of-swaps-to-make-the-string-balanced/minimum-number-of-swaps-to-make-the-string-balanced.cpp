7bd3a3f74be23ac38c42f40435b818fd3821e13e
SouravAgarwal69 <souravagarwal22072003@gmail.com>
2026-05-23 12:41:43 +0530

    Time: 20 ms (28.15%) | Memory: 36.8 MB (24.67%) - LeetSync

--- 2095-minimum-number-of-swaps-to-make-the-string-balanced/minimum-number-of-swaps-to-make-the-string-balanced.cpp
+++ 2095-minimum-number-of-swaps-to-make-the-string-balanced/minimum-number-of-swaps-to-make-the-string-balanced.cpp
@@ -4,15 +4,15 @@ public:
         stack<char>st;
         for(int i=0;i<s.size();i++)
         {
-            if(!st.empty() && st.top()=='[' && s[i]==']')
-            {
-                st.pop();
-            }
-            else if(s[i]=='[')
+            if(s[i]=='[')
             {
                 st.push(s[i]);
             }
+            else if(s[i]==']' && !st.empty())
+            {
+                st.pop();
+            }
         }
-        return st.empty()?0:(st.size()+1)/2;
+        return (st.size()+1)/2;
     }
 };
