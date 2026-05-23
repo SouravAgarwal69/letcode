acbd770dbf203f6a6a3050b5e4e571c7c52e8077
SouravAgarwal69 <souravagarwal22072003@gmail.com>
2026-05-23 09:34:35 +0530

    Time: 39 ms (27.69%) | Memory: 25.5 MB (61.02%) - LeetSync

--- 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.cpp
+++ 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.cpp
@@ -1,31 +1,27 @@
 class Solution {
 public:
     int minimumDeletions(string s) {
-        vector<int>rightA(s.size());
-        vector<int>leftB(s.size());
-        int cnt=0;
+        int cnt=s.size();
+        int cntA=0,cntB=0;
         for(int i=0;i<s.size();i++)
         {
-            leftB[i]=cnt;
-            if(s[i]=='b')
-            {
-                cnt++;
-            }
-        }
-        cnt=0;
-        for(int i=s.size()-1;i>=0;i--)
-        {
-            rightA[i]=cnt;
             if(s[i]=='a')
             {
-                cnt++;
+                cntA++;
             }
         }
-        int result=s.size();
         for(int i=0;i<s.size();i++)
         {
-            result=min(result,leftB[i]+rightA[i]);
+            if(s[i]=='a')
+            {
+                cntA--;
+            }
+            cnt=min(cnt,cntA+cntB);
+            if(s[i]=='b')
+            {
+                cntB++;
+            }
         }
-        return result;
+        return cnt;
     }
 };
