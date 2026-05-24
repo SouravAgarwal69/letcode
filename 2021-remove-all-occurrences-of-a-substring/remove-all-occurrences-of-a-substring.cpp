41a65013abddf9e5cb9cb2b44f0a8d0f7a2792c9
SouravAgarwal69 <souravagarwal22072003@gmail.com>
2026-05-24 07:50:44 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (6.95%) - LeetSync

--- 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.cpp
+++ 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     string removeOccurrences(string s, string part) {
-       string result;
-          for(int i=0;i<s.size();i++)
-          {
-             result.push_back(s[i]);
-             if(result.size()>=part.size() && result.substr(result.size()-part.size())==part)
-             {
+        string result;
+        for(int i=0;i<s.size();i++)
+        {
+            result.push_back(s[i]);
+            if(result.size()>=part.size() && result.substr(result.size()-part.size())==part)
+            {
                 result.erase(result.size()-part.size());
-             }
-          }
+            }
+        }
         return result;
     }
 };
