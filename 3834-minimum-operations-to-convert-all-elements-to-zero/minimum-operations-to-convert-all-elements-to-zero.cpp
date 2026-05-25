3abb2edf1a2375b2b85b2dd82638392c67b4bece
SouravAgarwal69 <souravagarwal22072003@gmail.com>
2026-05-25 18:06:14 +0530

    Time: 50 ms (22.34%) | Memory: 222.1 MB (65.93%) - LeetSync

--- 3834-minimum-operations-to-convert-all-elements-to-zero/minimum-operations-to-convert-all-elements-to-zero.cpp
+++ 3834-minimum-operations-to-convert-all-elements-to-zero/minimum-operations-to-convert-all-elements-to-zero.cpp
@@ -5,19 +5,18 @@ public:
         stack<int>st;
         for(int i=0;i<nums.size();i++)
         {
-           
-            while(!st.empty() && st.top()>nums[i])
-            {
-                st.pop();
-            }
+           while(!st.empty() && st.top()>nums[i])
+           {
+              st.pop();
+           }
              if(nums[i]==0)
             {
                 continue;
             }
             if(st.empty() || st.top()<nums[i])
             {
-                operation++;
-                st.push(nums[i]);
+              st.push(nums[i]);
+              operation++;
             }
         }
         return operation;
