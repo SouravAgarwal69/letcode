e45f9300aac7c4de92ae8c0244e5d02ff35977b9
SouravAgarwal69 <souravagarwal22072003@gmail.com>
2026-05-27 00:45:47 +0530

    Time: 35 ms (49.35%) | Memory: 222 MB (82.26%) - LeetSync

--- 3834-minimum-operations-to-convert-all-elements-to-zero/minimum-operations-to-convert-all-elements-to-zero.cpp
+++ 3834-minimum-operations-to-convert-all-elements-to-zero/minimum-operations-to-convert-all-elements-to-zero.cpp
@@ -1,24 +1,24 @@
 class Solution {
 public:
     int minOperations(vector<int>& nums) {
-        int operation=0;
+        int ops=0;
         stack<int>st;
         for(int i=0;i<nums.size();i++)
         {
-           while(!st.empty() && st.top()>nums[i])
-           {
-              st.pop();
-           }
-             if(nums[i]==0)
+            while(!st.empty() && st.top()>nums[i])
+            {
+                st.pop();
+            }
+            if(nums[i]==0)
             {
                 continue;
             }
             if(st.empty() || st.top()<nums[i])
             {
-              st.push(nums[i]);
-              operation++;
+                st.push(nums[i]);
+                ops++;
             }
         }
-        return operation;
+        return ops;
     }
 };
