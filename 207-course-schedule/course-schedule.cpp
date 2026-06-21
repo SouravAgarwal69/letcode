b01e270ebe6da5286f503234d7dd236178fb63e1
SouravAgarwal69 <souravagarwal22072003@gmail.com>
2026-06-21 06:31:32 +0530

    Time: 0 ms (100.00%) | Memory: 18.5 MB (96.33%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -1,45 +1,39 @@
 class Solution {
 public:
-    bool detectCycle(int node,vector<int>adj[],vector<bool>&visited,vector<bool>&path)
-    {
-        visited[node]=true;
-        path[node]=true;
-        for(int i=0;i<adj[node].size();i++)
-        {
-            if(visited[adj[node][i]])
-            {
-                if(path[adj[node][i]])
-                {
-                    return true;
-                }
-            }
-            else
-            {
-                if(detectCycle(adj[node][i],adj,visited,path))
-                {
-                    return true;
-                }
-            }
-        }
-        path[node]=false;
-        return false;
-    }
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        int V=numCourses;
-        vector<int>adj[V];
-        vector<bool>visited(V);
-        vector<bool>path(V);
+        vector<int>adj[numCourses];
+        vector<int>indegree(numCourses);
+        queue<int>q;
+          int cntCourse=0;
+           
         for(int i=0;i<prerequisites.size();i++)
         {
-            adj[prerequisites[i][1]].push_back(prerequisites[i][0]);
+              indegree[prerequisites[i][0]]++;
+              adj[prerequisites[i][1]].push_back(prerequisites[i][0]);
         }
-        for(int i=0;i<V;i++)
-        {
-              if(!visited[i] && detectCycle(i,adj,visited,path))
-              {
-                 return false;
-              }
-        }
-        return true;
+
+       for(int i=0;i<numCourses;i++)
+       {
+          if(indegree[i]==0)
+          {
+            q.push(i);
+            cntCourse++;
+          }
+       }
+       while(!q.empty())
+       {
+           int node=q.front();
+           q.pop();
+           for(int i=0;i<adj[node].size();i++)
+           {
+                indegree[adj[node][i]]--;
+               if(indegree[adj[node][i]]==0)
+               {
+                   cntCourse++;
+                   q.push(adj[node][i]);
+               }
+           }
+       }
+       return cntCourse==numCourses;
     }
 };
