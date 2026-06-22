ac6a20e676e727d0437a3c094b5111dbe6045469
SouravAgarwal69 <souravagarwal22072003@gmail.com>
2026-06-22 07:12:55 +0530

    Time: 0 ms (100.00%) | Memory: 18.5 MB (96.33%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -1,39 +1,46 @@
 class Solution {
 public:
+    bool dfs(vector<bool>&visited,vector<bool>&path,vector<int>adj[],int node)
+    {
+            visited[node]=true;
+            path[node]=true;
+            for(int i=0;i<adj[node].size();i++)
+            {
+                if(visited[adj[node][i]])
+                {
+                    if(path[adj[node][i]])
+                    {
+                        return true;
+                    }
+                }
+                else{
+                   if(dfs(visited,path,adj,adj[node][i]))
+                   {
+                       return true;
+                   }
+                }
+            }
+            path[node]=false;
+            return false;
+    }
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
         vector<int>adj[numCourses];
-        vector<int>indegree(numCourses);
-        queue<int>q;
-          int cntCourse=0;
-           
+        vector<bool>visited(numCourses);
         for(int i=0;i<prerequisites.size();i++)
         {
-              indegree[prerequisites[i][0]]++;
-              adj[prerequisites[i][1]].push_back(prerequisites[i][0]);
+            adj[prerequisites[i][1]].push_back(prerequisites[i][0]);
         }
-
-       for(int i=0;i<numCourses;i++)
-       {
-          if(indegree[i]==0)
-          {
-            q.push(i);
-            cntCourse++;
-          }
-       }
-       while(!q.empty())
-       {
-           int node=q.front();
-           q.pop();
-           for(int i=0;i<adj[node].size();i++)
-           {
-                indegree[adj[node][i]]--;
-               if(indegree[adj[node][i]]==0)
-               {
-                   cntCourse++;
-                   q.push(adj[node][i]);
-               }
-           }
-       }
-       return cntCourse==numCourses;
+       vector<bool>path(numCourses);
+        for(int i=0;i<numCourses;i++)
+        {
+            if(!visited[i])
+            {
+                if(dfs(visited,path,adj,i))
+                {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 };
