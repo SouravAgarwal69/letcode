bd162848f394a985e1895332b63411f079f08bd0
SouravAgarwal69 <souravagarwal22072003@gmail.com>
2026-06-26 16:59:00 +0530

    Time: 7 ms (72.77%) | Memory: 42.1 MB (92.63%) - LeetSync

--- 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.cpp
+++ 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.cpp
@@ -1,60 +1,51 @@
 class Solution {
 public:
-    int find(int x,vector<int>&parent)
+    int find(int node,vector<int>&parent)
     {
-        if(x==parent[x])
+        if(node==parent[node])
         {
-            return x;
+            return node;
         }
-        return parent[x]=find(parent[x],parent);
+        return parent[node]=find(parent[node],parent);
     }
-    void Union(int x,int y,vector<int>&rank,vector<int>&parent)
+    void Union(int x,int y,vector<int>&parent,vector<int>&rank)
     {
-        int x_parent=find(x,parent);
-        int y_parent=find(y,parent);
-        if(x_parent==y_parent)
+        if(rank[x]==rank[y])
         {
-            return;
+            parent[x]=y;
+            rank[y]++;
         }
-        if(rank[x_parent]>rank[y_parent])
+        else if(rank[x]>rank[y])
         {
-            parent[y_parent]=x_parent;
-        }
-        else if(rank[x_parent]<rank[y_parent])
-        {
-            parent[x_parent]=y_parent;
+            parent[y]=x;
         }
         else
         {
-            parent[y_parent]=x_parent;
-            rank[x_parent]++;
+            parent[x]=y;
         }
-
     }
     int makeConnected(int n, vector<vector<int>>& connections) {
-        if((n-1)>connections.size())
+        if(connections.size()<n-1)
         {
             return -1;
         }
         int component=n;
         vector<int>parent(n);
-        vector<int>rank(n,0);
+        vector<int>rank(n);
         for(int i=0;i<n;i++)
         {
             parent[i]=i;
         }
         for(int i=0;i<connections.size();i++)
         {
-            int x=connections[i][0];
-            int y=connections[i][1];
-            int x_parent=find(x,parent);
-            int y_parent=find(y,parent);
-            if(x_parent==y_parent)
+            int x=find(connections[i][0],parent);
+            int y=find(connections[i][1],parent);
+            if(x==y)
             {
                 continue;
             }
-            Union(x_parent,y_parent,rank,parent);
-           component--;
+            Union(x,y,parent,rank);
+            component--;
         }
         return component-1;
     }
