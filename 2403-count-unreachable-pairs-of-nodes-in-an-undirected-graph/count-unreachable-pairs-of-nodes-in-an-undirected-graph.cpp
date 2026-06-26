455f1a7d31f418ae9229b2e7940e4bda4b0587a7
SouravAgarwal69 <souravagarwal22072003@gmail.com>
2026-06-26 18:18:20 +0530

    Time: 77 ms (65.31%) | Memory: 166.9 MB (56.92%) - LeetSync

--- 2403-count-unreachable-pairs-of-nodes-in-an-undirected-graph/count-unreachable-pairs-of-nodes-in-an-undirected-graph.cpp
+++ 2403-count-unreachable-pairs-of-nodes-in-an-undirected-graph/count-unreachable-pairs-of-nodes-in-an-undirected-graph.cpp
@@ -1,34 +1,28 @@
 class Solution {
 public:
-    int find(int node,vector<int>&parent)
+    int find(int x,vector<int>&parent)
     {
-        if(node==parent[node])
+        if(x==parent[x])
         {
-            return node;
+            return x;
         }
-        return parent[node]=find(parent[node],parent);
+        return parent[x]=find(parent[x],parent);
     }
     void Union(int x,int y,vector<int>&parent,vector<int>&rank)
     {
-        int x_parent=find(x,parent);
-        int y_parent=find(y,parent);
-        if(x_parent==y_parent)
-        {
-            return;
-        }
-        if(rank[x_parent]==rank[y_parent])
-        {
-            parent[y_parent]=x_parent;
-            rank[x_parent]++;
-        }
-        else if(rank[x_parent]>rank[y_parent])
-        {
-            parent[y_parent]=x_parent;
-        }
-        else
-        {
-            parent[x_parent]=y_parent;
-        }
+         if(rank[x]==rank[y])
+         {
+            parent[x]=y;
+            rank[y]++;
+         }
+         else if(rank[x]>rank[y])
+         {
+            parent[y]=x;
+         }
+         else
+         {
+            parent[x]=y;
+         }
     }
     long long countPairs(int n, vector<vector<int>>& edges) {
         vector<int>parent(n);
@@ -37,24 +31,40 @@ public:
         {
             parent[i]=i;
         }
+        vector<int>adj[n];
         for(int i=0;i<edges.size();i++)
         {
-            Union(edges[i][0],edges[i][1],parent,rank);
+            int u=edges[i][0];
+            int v=edges[i][1];
+            adj[u].push_back(v);
+        }
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<adj[i].size();j++)
+            {
+                int x=find(i,parent);
+                int y=find(adj[i][j],parent);
+                if(x==y)
+                {
+                    continue;
+                }
+                Union(x,y,parent,rank);
+            }
         }
         unordered_map<int,int>mp;
         for(int i=0;i<n;i++)
         {
-            int leader=find(i,parent);
-            mp[leader]++;
+            int p=find(i,parent);
+            mp[p]++;
         }
-        int neighbour=n;
         long long result=0;
+        int neighbour=n;
         for(auto it=mp.begin();it!=mp.end();it++)
         {
             int size=it->second;
-            result=result+(long long)size*(neighbour-size);
+            result=result+(long long) size*(neighbour-size);
             neighbour=neighbour-size;
         }
-        return result;
+       return result;
     }
 };
