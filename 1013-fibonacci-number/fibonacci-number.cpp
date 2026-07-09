eafa4902f5a78f988a259d492e26ed33e0e7f5ed
SouravAgarwal69 <souravagarwal22072003@gmail.com>
2026-07-09 21:02:56 +0530

    Time: 15 ms (23.03%) | Memory: 7.9 MB (37.93%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,20 +1,10 @@
 class Solution {
 public:
-    int dp[31];
-    int find(int n)
-    {
-        if(n<2)
+    int fib(int n) {
+        if(n==0 || n==1)
         {
             return n;
         }
-        if(dp[n]!=-1)
-        {
-            return dp[n];
-        }
-        return dp[n]=find(n-1)+find(n-2);
-    }
-    int fib(int n) {
-        memset(dp,-1,sizeof(dp));
-        return find(n);
+        return fib(n-1)+fib(n-2);
     }
 };
