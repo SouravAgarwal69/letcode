420ae2e333273419c9d61fc5e04334796eeb5d28
SouravAgarwal69 <souravagarwal22072003@gmail.com>
2026-07-12 19:22:12 +0530

    Time: 0 ms (100.00%) | Memory: 17.5 MB (58.85%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.cpp
+++ 801-is-graph-bipartite/is-graph-bipartite.cpp
@@ -1,46 +1,34 @@
 class Solution {
 public:
-    bool bfs(vector<int>adj[],vector<int>&color,int node)
+    bool dfs(int node,vector<int>&color,vector<vector<int>>&graph)
     {
-        queue<int>q;
-        q.push(node);
-        while(!q.empty())
-        {
-            int node=q.front();
-            q.pop();
-            for(int i=0;i<adj[node].size();i++)
-            {
-                if(color[adj[node][i]]==-1)
-                {
-                    color[adj[node][i]]=1-color[node];
-                    q.push(adj[node][i]);
-                }
-                else if(color[adj[node][i]]==color[node])
-                {
+          for(int i=0;i<graph[node].size();i++)
+          {
+             if(color[graph[node][i]]==-1)
+             {
+                 color[graph[node][i]]=1-color[node];
+                 if(!dfs(graph[node][i],color,graph))
+                 {
                     return false;
-                }
-            }
-        }
-        return true;
+                 }
+             }
+             else if(color[node]==color[graph[node][i]])
+             {
+                return false;
+             }
+          }
+          return true;
     }
     bool isBipartite(vector<vector<int>>& graph) {
         vector<int>color(graph.size(),-1);
-        vector<int>adj[graph.size()];
-        for(int i=0;i<graph.size();i++)
-        {
-            for(int j=0;j<graph[i].size();j++)
-            {
-                adj[i].push_back(graph[i][j]);
-            }
-        }
         for(int i=0;i<graph.size();i++)
         {
             if(color[i]==-1)
             {
                 color[i]=0;
-                if(!bfs(adj,color,i))
+                if(!dfs( i,color,graph))
                 {
-                    return false;
+                   return false;
                 }
             }
         }
