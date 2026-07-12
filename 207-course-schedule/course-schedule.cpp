6738799ad5db76a192f7c21a70e954d9aff6e1bd
SouravAgarwal69 <souravagarwal22072003@gmail.com>
2026-07-12 14:54:53 +0530

    Time: 0 ms (100.00%) | Memory: 18.9 MB (92.86%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -1,37 +1,51 @@
 class Solution {
 public:
-    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<int>result;
-        vector<int>indegree(numCourses);
-        vector<int>adj[numCourses];
-        for(int i=0;i<prerequisites.size();i++)
+    bool dfs(vector<int>adj[],stack<int>&st,vector<bool>&visited,int node,vector<bool>&path)
+    {
+        visited[node]=true;
+        path[node]=true;
+        for(int i=0;i<adj[node].size();i++)
         {
-            adj[prerequisites[i][1]].push_back(prerequisites[i][0]);
-            indegree[prerequisites[i][0]]++;
-        }
-        queue<int>q;
-        for(int i=0;i<numCourses;i++)
-        {
-            if(indegree[i]==0)
+            if(visited[adj[node][i]])
             {
-                q.push(i);
-                result.push_back(i);
+               if(path[adj[node][i]])
+               {
+                  return true;
+               }
             }
-        }
-        while(!q.empty())
-        {
-            int node=q.front();
-            q.pop();
-            for(int i=0;i<adj[node].size();i++)
+            else if(!visited[adj[node][i]])
             {
-                indegree[adj[node][i]]--;
-                if(indegree[adj[node][i]]==0)
+                if(dfs(adj,st,visited,adj[node][i],path))
                 {
-                    result.push_back(adj[node][i]);
-                    q.push(adj[node][i]);
+                    return true;
                 }
             }
         }
-        return result.size()==numCourses;
+        st.push(node);
+        path[node]=false;
+        return false;
+    }
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<int>adj[numCourses];
+        for(int i=0;i<prerequisites.size();i++)
+        {
+            int u=prerequisites[i][0];
+            int v=prerequisites[i][1];
+            adj[v].push_back(u);
+        }
+        stack<int>st;
+        vector<bool>visited(numCourses);
+        vector<bool>path(numCourses);
+        for(int i=0;i<numCourses;i++)
+        {
+            if(!visited[i])
+            {
+                if(dfs(adj,st,visited,i,path))
+                {
+                    return false;
+                }
+            }
+        }
+        return st.size()==numCourses;
     }
 };
