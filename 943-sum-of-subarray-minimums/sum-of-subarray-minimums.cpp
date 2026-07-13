48d7fee1fa626fea26952313ac396074493526e7
SouravAgarwal69 <souravagarwal22072003@gmail.com>
2026-07-13 18:54:41 +0530

    Time: 31 ms (68.51%) | Memory: 193.7 MB (10.85%) - LeetSync

--- 943-sum-of-subarray-minimums/sum-of-subarray-minimums.cpp
+++ 943-sum-of-subarray-minimums/sum-of-subarray-minimums.cpp
@@ -1,58 +1,58 @@
 class Solution {
 public:
-    void findNSE(vector<int>&arr,vector<int>&NSE)
-    {
-          stack<int>st;
-          for(int i=arr.size()-1;i>=0;i--)
-          {
-             while(!st.empty() && arr[st.top()]>=arr[i])
-             {
-                st.pop();
-             } 
-             if(st.empty())
-             {
-                NSE[i]=arr.size();
-             }
-             else
-             {
-                NSE[i]=st.top();
-             }
-             st.push(i);
-          }
-    }
-    void findPSE(vector<int>&arr,vector<int>&PSE)
+    void findLeft(vector<int>&arr,vector<int>&left)
     {
          stack<int>st;
          for(int i=0;i<arr.size();i++)
          {
-            while(!st.empty() && arr[st.top()]>arr[i])
-            {
+             while(!st.empty() && arr[st.top()]>=arr[i])
+             {
                 st.pop();
-            }
-            if(st.empty())
-            {
-                PSE[i]=-1;
-            }
-            else
-            {
-                PSE[i]=st.top();
-            }
-            st.push(i);
+             }
+             if(st.empty())
+             {
+                left[i]=-1;
+             }
+             else
+             {
+                left[i]=st.top();
+             }
+             st.push(i);  
          }
+    }
+    void findRight(vector<int>&arr,vector<int>&right)
+    {
+        stack<int>st;
+        for(int i=arr.size()-1;i>=0;i--)
+        {
+           while(!st.empty() && arr[st.top()]>arr[i])
+           {
+              st.pop();
+           }
+           if(st.empty())
+           {
+              right[i]=arr.size();
+           }
+           else 
+           {
+              right[i]=st.top();
+           }
+           st.push(i);
+        }
     }
     int sumSubarrayMins(vector<int>& arr) {
         int mod=1e9+7;
-        vector<int>NSE(arr.size());
-        vector<int>PSE(arr.size());
-        findNSE(arr,NSE);
-        findPSE(arr,PSE);
-        int total=0;
+        vector<int>left(arr.size());
+        vector<int>right(arr.size());
+        findLeft(arr,left);
+        findRight(arr,right);
+        long long sum=0;
         for(int i=0;i<arr.size();i++)
         {
-           long long left=i-PSE[i];
-           long long right=NSE[i]-i;
-           total=(total+(left*right % mod)*arr[i] % mod) % mod;
+           int leftSum=i-left[i];
+           int rightSum=right[i]-i;
+            sum=(sum+1LL*leftSum*rightSum*arr[i])%mod;
         }
-        return total;
+        return sum%mod;
     }
 };
