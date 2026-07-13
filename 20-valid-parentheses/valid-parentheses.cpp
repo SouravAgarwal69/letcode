9f4b0b0af7adffee74e32831170e9e9ef968a5ef
SouravAgarwal69 <souravagarwal22072003@gmail.com>
2026-07-13 22:07:51 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (67.84%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -4,15 +4,15 @@ public:
         stack<char>st;
         for(int i=0;i<s.size();i++)
         {
-            if(!st.empty() && ((st.top()=='(' && s[i]==')') || (st.top()=='{' && s[i]=='}') || (st.top()=='[' && s[i]==']')))
-            {
-                st.pop();
-            }
+                if(!st.empty() && ((st.top()=='(' && s[i]==')') || (st.top()=='{' && s[i]=='}') || (st.top()=='[' && s[i]==']')))
+                {
+                   st.pop();
+                }
             else
             {
                 st.push(s[i]);
             }
         }
-        return st.empty();
+       return st.empty();
     }
 };
