d7552709679b70b5f3f829033eb8fa9c815772cc
SouravAgarwal69 <souravagarwal22072003@gmail.com>
2026-07-14 04:09:21 +0530

    Time: 7 ms (17.87%) | Memory: 23.8 MB (5.01%) - LeetSync

--- 71-simplify-path/simplify-path.cpp
+++ 71-simplify-path/simplify-path.cpp
@@ -1,36 +1,36 @@
 class Solution {
 public:
     string simplifyPath(string path) {
-       vector<string>store;
-       stringstream ss(path);
-       string result;
-       string temp;
-       while(getline(ss,temp,'/'))
-       {
-           if(temp=="." || temp=="")
-           {
-             continue;
-           }
-           if(temp=="..")
-           {
-             if(!store.empty())
+        vector<string>store;
+        string temp;
+        stringstream ss(path);
+        while(getline(ss,temp,'/'))
+        {
+             if(temp=="." || temp=="")
              {
-                store.pop_back();
+                continue;
              }
-           }
-           else
-           {
-              store.push_back(temp);
-           }
-       }
-       if(store.empty())
-       {
-          return "/";
-       }
-     for(int i=0;i<store.size();i++)
-     {
-         result=result+"/"+store[i];
-     }
-      return result;
+             else if(temp=="..")
+             {
+                if(!store.empty())
+                {
+                    store.pop_back();
+                }
+             }
+             else
+             {
+                store.push_back(temp);
+             }
+        }
+        if(store.empty())
+        {
+            return "/";
+        }
+        string result;
+        for(int i=0;i<store.size();i++)
+        {
+             result=result+"/"+store[i];
+        }
+        return result;
     }
 };
