2b0e2f61bea58e561bd7f1a564a2eadbc1209d05
SouravAgarwal69 <souravagarwal22072003@gmail.com>
2026-07-13 11:38:47 +0530

    Time: 47 ms (70.66%) | Memory: 151.1 MB (70.50%) - LeetSync

--- 2403-count-unreachable-pairs-of-nodes-in-an-undirected-graph/count-unreachable-pairs-of-nodes-in-an-undirected-graph.cpp
+++ 2403-count-unreachable-pairs-of-nodes-in-an-undirected-graph/count-unreachable-pairs-of-nodes-in-an-undirected-graph.cpp
@@ -1,28 +1,28 @@
 class Solution {
 public:
-    int find(int x,vector<int>&parent)
+    int find(int node,vector<int>&parent)
     {
-        if(x==parent[x])
+        if(node==parent[node])
         {
-            return x;
+            return node;
         }
-        return parent[x]=find(parent[x],parent);
+        return parent[node]=find(parent[node],parent);
     }
     void Union(int x,int y,vector<int>&parent,vector<int>&rank)
     {
-         if(rank[x]==rank[y])
-         {
+        if(rank[x]==rank[y])
+        {
             parent[x]=y;
             rank[y]++;
-         }
-         else if(rank[x]>rank[y])
-         {
+        }
+        else if(rank[x]>rank[y])
+        {
             parent[y]=x;
-         }
-         else
-         {
+        }
+        else 
+        {
             parent[x]=y;
-         }
+        }
     }
     long long countPairs(int n, vector<vector<int>>& edges) {
         vector<int>parent(n);
@@ -37,26 +37,25 @@ public:
             int v=edges[i][1];
             int x=find(u,parent);
             int y=find(v,parent);
-                if(x==y)
-                {
-                    continue;
-                }
-                Union(x,y,parent,rank);
+            if(x==y)
+            {
+                continue;
             }
+            Union(x,y,parent,rank);
+        }
         unordered_map<int,int>mp;
         for(int i=0;i<n;i++)
         {
-            int p=find(i,parent);
-            mp[p]++;
+            mp[find(i,parent)]++;
         }
         long long result=0;
         int neighbour=n;
         for(auto it=mp.begin();it!=mp.end();it++)
         {
             int size=it->second;
-            result=result+(long long) size*(neighbour-size);
+            result=result+(long long)(neighbour-size)*size;
             neighbour=neighbour-size;
         }
-       return result;
+        return result;
     }
 };
