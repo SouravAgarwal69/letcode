50399f0cd4732dbae50a1ed97e1e1e883cb90815
SouravAgarwal69 <souravagarwal22072003@gmail.com>
2026-07-13 21:28:34 +0530

    Time: 2 ms (41.57%) | Memory: 19.2 MB (6.16%) - LeetSync

--- 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.cpp
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.cpp
@@ -1,37 +1,38 @@
 class Solution {
 public:
     int evalRPN(vector<string>& tokens) {
-        stack<int>st;
-        for(int i=0;i<tokens.size();i++)
-        {
-            if(tokens[i]=="+" || tokens[i]=="-" || tokens[i]=="*" || tokens[i]=="/")
-            {
-                int second=st.top();
-                st.pop();
-                int first=st.top();
-                st.pop();
-                if(tokens[i]=="+")
-                {
-                    st.push(first+second);
-                }
-                else if(tokens[i]=="-")
-                {
-                    st.push(first-second);
-                }
-                else if(tokens[i]=="*")
-                {
-                    st.push(first*second);
-                }
-                else
-                {
-                    st.push(first/second);
-                }
-            }
-            else
-            {
-                st.push(stoi(tokens[i]));
-            }
-        }
-        return st.top();
+       int result=0;
+       stack<string>st;
+       for(int i=0;i<tokens.size();i++)
+       {
+           if(tokens[i]=="+" || tokens[i]=="-" || tokens[i]=="*" || tokens[i]=="/")
+           {
+               int a=stoi(st.top());
+               st.pop();
+               int b=stoi(st.top());
+               st.pop();
+               if(tokens[i]=="+")
+               {
+                   st.push(to_string(a+b));
+               }
+               else if(tokens[i]=="-")
+               {
+                  st.push(to_string(b-a));
+               }
+               else if(tokens[i]=="*")
+               {
+                  st.push(to_string(a*b));
+               }
+               else
+               {
+                   st.push(to_string(b/a));
+               }
+           }
+           else
+           {
+              st.push(tokens[i]);
+           }
+       } 
+       return stoi(st.top());
     }
 };
