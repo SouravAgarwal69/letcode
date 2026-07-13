6af0fe3331cc20be80891f17d906f424416019d7
SouravAgarwal69 <souravagarwal22072003@gmail.com>
2026-07-13 15:46:25 +0530

    Time: 87 ms (44.57%) | Memory: 43.9 MB (94.83%) - LeetSync

--- 744-network-delay-time/network-delay-time.cpp
+++ 744-network-delay-time/network-delay-time.cpp
@@ -1,43 +1,43 @@
 class Solution {
 public:
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
-        vector<int>result(n+1,INT_MAX);
+        vector<int>Time(n+1,INT_MAX);
         vector<pair<int,int>>adj[n+1];
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
+        Time[k]=0;
+        pq.push({0,k});
         for(int i=0;i<times.size();i++)
         {
             int u=times[i][0];
             int v=times[i][1];
             int w=times[i][2];
-            adj[u].push_back({v,w});
+            adj[u].push_back({w,v});
         }
-        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
-        pq.push({0,k});
-        result[k]=0;
         while(!pq.empty())
         {
             int node=pq.top().second;
-            int times=pq.top().first;
+            int dist=pq.top().first;
             pq.pop();
-            if(times>result[node])
-            {
-                continue;
-            }
             for(int i=0;i<adj[node].size();i++)
             {
-                 int neigh=adj[node][i].first;
-                 int wt=adj[node][i].second;
-                 if(times+wt<result[neigh])
-                 {
-                    result[neigh]=times+wt;
-                    pq.push({times+wt,neigh});
-                 }
+                int wt=adj[node][i].first;
+                int neigh=adj[node][i].second;
+                if(dist+wt<Time[neigh])
+                {
+                    Time[neigh]=dist+wt;
+                    pq.push({wt+dist,neigh});
+                }
             }
         }
-        int DelayTime=INT_MIN;;
+        int result=0;
         for(int i=1;i<n+1;i++)
         {
-            DelayTime=max(DelayTime,result[i]);
+           if(Time[i]==INT_MAX)
+           {
+              return -1;
+           }
+           result=max(result,Time[i]);
         }
-        return DelayTime==INT_MAX?-1:DelayTime;
+        return result==INT_MAX?-1:result;
     }
 };
