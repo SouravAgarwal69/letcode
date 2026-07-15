7eff8f08cf5105913542da28214d1f05a8ddb3de
SouravAgarwal69 <souravagarwal22072003@gmail.com>
2026-07-16 04:59:44 +0530

    Time: 59 ms (59.76%) | Memory: 196.7 MB (67.25%) - LeetSync

--- 2846-robot-collisions/robot-collisions.cpp
+++ 2846-robot-collisions/robot-collisions.cpp
@@ -4,51 +4,49 @@ public:
         vector<int>result;
         vector<int>index(positions.size());
         iota(index.begin(),index.end(),0);
-        sort(index.begin(),index.end(),[&](int i,int j)
-        {
+        sort(index.begin(),index.end(),[&](int i,int j){
             return positions[i]<positions[j];
-        });
+        }
+        );
         stack<int>st;
         for(int i=0;i<index.size();i++)
         {
             bool collision=false;
             while(!st.empty() && directions[st.top()]=='R' && directions[index[i]]=='L')
             {
-                if(healths[st.top()]==healths[index[i]])
+                if(healths[index[i]]==healths[st.top()])
                 {
-                    
-                    healths[st.top()]=0;
                     healths[index[i]]=0;
+                    healths[st.top()]=0;
                     st.pop();
                     collision=true;
                     break;
                 }
-                else if(healths[st.top()]>healths[index[i]])
+                else if(healths[index[i]]>healths[st.top()])
                 {
-                    collision=true;
+                    healths[index[i]]-=1;
+                    healths[st.top()]=0;
+                    st.pop();
+                }
+                else if(healths[index[i]]<healths[st.top()])
+                {
                     healths[index[i]]=0;
                     healths[st.top()]-=1;
+                    collision=true;
                     break;
-                }
-                else
-                {
-                    healths[st.top()]=0;
-                    healths[index[i]]-=1;
-                      st.pop();
                 }
             }
             if(!collision)
-            {
-                st.push(index[i]);
-            }
+                {
+                    st.push(index[i]);
+                }
         }
-        for(int i=0;i<healths.size();i++)
+        for(int i=0;i<positions.size();i++)
         {
-            if(healths[i]==0)
+            if(healths[i]!=0)
             {
-                continue;
+                result.push_back(healths[i]);
             }
-            result.push_back(healths[i]);
         }
         return result;
     }
