7ac6511d0dc43bc7012fba27316ae40e8f684763
SouravAgarwal69 <souravagarwal22072003@gmail.com>
2026-07-15 09:26:35 +0530

    Time: 4 ms (12.20%) | Memory: 32.2 MB (44.73%) - LeetSync

--- 1484-linked-list-in-binary-tree/linked-list-in-binary-tree.cpp
+++ 1484-linked-list-in-binary-tree/linked-list-in-binary-tree.cpp
@@ -21,7 +21,7 @@
  */
 class Solution {
 public:
-    bool check(ListNode*head,TreeNode*root)
+    bool check(ListNode* head,TreeNode* root)
     {
         if(!head)
         {
@@ -31,20 +31,20 @@ public:
         {
             return false;
         }
-        if(root->val !=head->val)
+        if(head->val!=root->val)
         {
             return false;
         }
         return check(head->next,root->left) || check(head->next,root->right);
     }
     bool isSubPath(ListNode* head, TreeNode* root) {
-        if(!root)
-        {
-            return false;
-        }
         if(!head)
         {
             return true;
+        }
+        if(!root)
+        {
+            return false;
         }
         return check(head,root) || isSubPath(head,root->left) || isSubPath(head,root->right);
     }
