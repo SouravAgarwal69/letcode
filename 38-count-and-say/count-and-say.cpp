861efef74109bdeb39cd0c0e45cda86a82e83242
SouravAgarwal69 <souravagarwal22072003@gmail.com>
2026-07-19 12:19:38 +0530

    Time: 3 ms (81.39%) | Memory: 10.2 MB (25.14%) - LeetSync

--- 38-count-and-say/count-and-say.cpp
+++ 38-count-and-say/count-and-say.cpp
@@ -1,26 +1,25 @@
 class Solution {
 public:
     string countAndSay(int n) {
-        string temp="1";
-        for(int i=1;i<n;i++)
+        if(n==1)
         {
-            int j=0;
-            string ptr;
-            while(j<temp.size())
-            {
-
-                int cnt=1,k=j+1;
-                while(k<temp.size() && temp[j]==temp[k])
-                {
-                    cnt++;
-                    k++;
-                }
-                ptr+=to_string(cnt);
-                ptr+=temp[j];
-                 j=k;
-            }
-            temp=ptr;
+            return "1";
         }
-        return temp;
+        string temp=countAndSay(n-1);
+        int i=0;
+        string ptr;
+        while(i<temp.size())
+        {
+            int j=i+1,cnt=1;
+            while(j<temp.size() && temp[i]==temp[j])
+            {
+               cnt++;
+               j++;
+            }
+            ptr+=to_string(cnt);
+            ptr+=temp[i];
+              i=j;
+        }
+        return ptr;
     }
 };
