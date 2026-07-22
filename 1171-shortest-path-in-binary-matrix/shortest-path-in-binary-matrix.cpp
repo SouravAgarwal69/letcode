109fe5949c0b7302eabf07a4487ed840ee1d24db
SouravAgarwal69 <souravagarwal22072003@gmail.com>
2026-07-22 10:44:24 +0530

    Time: 11 ms (88.92%) | Memory: 23.5 MB (86.04%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
@@ -1,43 +1,44 @@
 class Solution {
 public:
-    bool check(int r,int c,int n)
+    bool check(int x,int y,int n)
     {
-        return r>=0 && r<n && c>=0 && c<n;
+        return x>=0 && x<n && y>=0 && y<n;
     }
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
-        int row[8]={-1,1,-1,1,-1,1,0,0};
-        int col[8]={0,0,-1,1,1,-1,1,-1};
         int n=grid.size();
         queue<pair<int,int>>q;
+        int cost=0;
         if(grid[0][0]==0)
         {
-        q.push({0,0});
+            q.push({0,0});
+            grid[0][0]=1;
         }
-        int level=0;
+        int r[8]={1,-1,0,0,1,-1,-1,1};
+        int c[8]={0,0,1,-1,1,-1,1,-1};
         while(!q.empty())
         {
             int size=q.size();
+             cost++;
             while(size>0)
             {
-                int r=q.front().first;
-            int c=q.front().second;
+            int row=q.front().first;
+            int col=q.front().second;
             q.pop();
-            if(r==n-1 && c==n-1)
+            if(row==n-1 && col==n-1)
             {
-                return level+1;
+                return cost;
             }
             for(int i=0;i<8;i++)
             {
-                if(  check(r+row[i],c+col[i],n) && grid[r+row[i]][c+col[i]]==0)
+                if(check(row+r[i],col+c[i],n) && grid[row+r[i]][col+c[i]]==0 )
                 {
-                    
-                        q.push({r+row[i],c+col[i]});
-                        grid[r+row[i]][c+col[i]]=1;
+                    grid[row+r[i]][col+c[i]]=1;
+                    q.push({row+r[i],col+c[i]});
                 }
             }
             size--;
             }
-            level++;
+           
         }
         return -1;
     }
