454039890825cbe1fc7fa9936e7c52991f1eb951
SouravAgarwal69 <souravagarwal22072003@gmail.com>
2026-07-22 14:51:49 +0530

    Time: 863 ms (13.59%) | Memory: 165.2 MB (22.88%) - LeetSync

--- 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.cpp
+++ 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.cpp
@@ -3,23 +3,23 @@ public:
     int minCostConnectPoints(vector<vector<int>>& points) {
         int v=points.size();
         vector<pair<int,int>>adj[v];
+        vector<bool>visited(v,0);
+        int minCost=0;
         for(int i=0;i<v;i++)
         {
             for(int j=i+1;j<v;j++)
             {
                 int x1=points[i][0],y1=points[i][1],x2=points[j][0],y2=points[j][1];
                 int dist=abs(x1-x2)+abs(y1-y2);
-                adj[i].push_back({dist,j});
-                adj[j].push_back({dist,i});
+                adj[i].push_back({j,dist});
+                adj[j].push_back({i,dist});
             }
-        } 
-        vector<bool>visited(v,false);
-        int totalCost=0;
+        }
         priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
         pq.push({0,0});
         while(!pq.empty())
         {
-            int dist=pq.top().first;
+            int cost=pq.top().first;
             int node=pq.top().second;
             pq.pop();
             if(visited[node])
@@ -27,17 +27,12 @@ public:
                 continue;
             }
             visited[node]=true;
-            totalCost+=dist;
+            minCost+=cost;
             for(int i=0;i<adj[node].size();i++)
             {
-                int neigh=adj[node][i].second;
-                int wt=adj[node][i].first;
-                if(!visited[neigh])
-                {
-                    pq.push({wt,neigh});
-                }
+                pq.push({adj[node][i].second,adj[node][i].first});
             }
         }
-        return totalCost;
+        return minCost;
     }
 };
