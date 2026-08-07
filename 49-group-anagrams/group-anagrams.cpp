4207a590074c0beb1870636c75b8bdc3c7119673
SouravAgarwal69 <souravagarwal22072003@gmail.com>
2026-08-07 17:21:52 +0530

    Time: 22 ms (31.02%) | Memory: 29.6 MB (18.39%) - LeetSync

--- 49-group-anagrams/group-anagrams.cpp
+++ 49-group-anagrams/group-anagrams.cpp
@@ -1,13 +1,29 @@
 class Solution {
 public:
+    void SortIt(string str,unordered_map<string,vector<string>>&mp)
+    {
+        vector<int>freq(26,0);
+        for(int i=0;i<str.size();i++)
+        {
+            freq[str[i]-'a']++;
+        }
+        string temp;
+        for(int i=0;i<26;i++)
+        {
+            while(freq[i]>0)
+            {
+                temp.push_back(i+'a');
+                freq[i]--;
+            }
+        }
+        mp[temp].push_back(str);
+    }
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         vector<vector<string>>result;
         unordered_map<string,vector<string>>mp;
         for(int i=0;i<strs.size();i++)
         {
-            string temp=strs[i];
-            sort(temp.begin(),temp.end());
-            mp[temp].push_back(strs[i]);
+            SortIt(strs[i],mp);
         }
         for(auto it=mp.begin();it!=mp.end();it++)
         {
