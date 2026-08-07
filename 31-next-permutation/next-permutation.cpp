59ee2334b606b459cf045e9ec06e7e2c10c1b940
SouravAgarwal69 <souravagarwal22072003@gmail.com>
2026-08-07 14:18:17 +0530

    Time: 0 ms (100.00%) | Memory: 15.8 MB (12.61%) - LeetSync

--- 31-next-permutation/next-permutation.cpp
+++ 31-next-permutation/next-permutation.cpp
@@ -1,26 +1,26 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        int index=-1;
-        for(int i=nums.size()-2;i>=0;i--)
+        int swapIndex=-1,n=nums.size();
+        for(int i=n-2;i>=0;i--)
         {
             if(nums[i]<nums[i+1])
             {
-                index=i;
+                swapIndex=i;
                 break;
             }
         }
-        if(index!=-1)
+        if(swapIndex!=-1)
         {
-            for(int i=nums.size()-1;i>index;i--)
-            {
-                if(nums[index]<nums[i])
-                {
-                    swap(nums[index],nums[i]);
-                    break;
-                }
-            }
+        for(int i=n-1;i>swapIndex;i--)
+        {
+              if(nums[i]>nums[swapIndex])
+              {
+                  swap(nums[i],nums[swapIndex]);
+                  break;
+              }
         }
-        reverse(nums.begin()+index+1,nums.end());
+        }
+        reverse(nums.begin()+swapIndex+1,nums.end());
     }
 };
