4214e13c3bf3db3603c407d51cf7f72769fec8d0
SouravAgarwal69 <souravagarwal22072003@gmail.com>
2026-08-09 13:01:37 +0530

    Time: 286 ms (7.02%) | Memory: 148.5 MB (25.58%) - LeetSync

--- 2117-find-original-array-from-doubled-array/find-original-array-from-doubled-array.cpp
+++ 2117-find-original-array-from-doubled-array/find-original-array-from-doubled-array.cpp
@@ -1,26 +1,26 @@
 class Solution {
 public:
     vector<int> findOriginalArray(vector<int>& changed) {
-        vector<int>result;
         if(changed.size()%2!=0)
         {
             return {};
         }
-        unordered_map<int,int>mp;
-        sort(changed.begin(),changed.end());
+        vector<int>result;
+        map<int,int>mp;
         for(int i=0;i<changed.size();i++)
         {
             mp[changed[i]]++;
         }
+        sort(changed.begin(),changed.end());
         for(int i=0;i<changed.size();i++)
         {
             if(mp[changed[i]]>0)
             {
-                if(mp[changed[i]*2]>0)
+                if(mp[2*changed[i]]>0)
                 {
+                    result.push_back(changed[i]);
                     mp[changed[i]]--;
                     mp[changed[i]*2]--;
-                    result.push_back(changed[i]);
                 }
                 else
                 {
