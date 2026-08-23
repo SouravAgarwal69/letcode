468df41d041852dfeb2292093d4d3fe71afcef97
SouravAgarwal69 <souravagarwal22072003@gmail.com>
2026-08-24 04:16:23 +0530

    Time: 0 ms (100.00%) | Memory: 15.1 MB (40.87%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
@@ -16,28 +16,25 @@ public:
         queue<TreeNode*>q;
         if(root)
         {
-        q.push(root);
+           q.push(root);
         }
         while(!q.empty())
         {
-           int size=q.size();
-           while(size--)
-           {
-              TreeNode*temp=q.front();
-              q.pop();
-              if(size==0)
-              {
-                   result.push_back(temp->val);
-              }
-               if(temp->left)
-               {
-                  q.push(temp->left);
-               }
-               if(temp->right)
-               {
-                   q.push(temp->right);
-               }
-           }
+            int size=q.size();
+            result.push_back(q.back()->val);
+            while(size--)
+            {
+                TreeNode* temp=q.front();
+                q.pop();
+                if(temp->left)
+                {
+                    q.push(temp->left);
+                }
+                if(temp->right)
+                {
+                    q.push(temp->right);
+                }
+            }
         }
         return result;
     }
